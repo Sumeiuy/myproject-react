f9b461751a5ab70c953ca976422008d2118e12ec
sunweibin <695170566@qq.com>
2017-04-25 09:04:49 +0800

    sunweibin:根据新版本UI设计稿进行修改原有代码

--- src/components/invest/PerformanceChartBoard.js
+++ src/components/invest/PerformanceChartBoard.js
@@ -4,12 +4,15 @@
  */
 
 import React, { PropTypes, PureComponent } from 'react';
-import { Button, Icon } from 'antd';
+import { Select } from 'antd';
 
+import Icon from '../common/Icon';
 import ChartBoard from './ChartBoard';
 
 
 import styles from './PerformanceChartBoard.less';
+
+const Option = Select.Option;
 
 export default class PerformanceChartBoard extends PureComponent {
 
@@ -27,11 +30,28 @@ export default class PerformanceChartBoard extends PureComponent {
       return null;
     }
     return (
-      <div>
+      <div className="investPerformanceBoard">
         <div className={styles.titleBar}>
           <div className={styles.titleText}>指标分布</div>
-          <div className={styles.exportBtn}>
-            <Button><Icon type="export" />导出</Button>
+          <div className={styles.titleBarRight}>
+            <div className={styles.iconBtn1}>
+              <span>排序方式:</span>
+              <Select defaultValue="1" className={styles.newSelect}>
+                <Option value="1">按分公司</Option>
+                <Option value="2">按营业部</Option>
+              </Select>
+              <Select defaultValue="1" className={styles.newSelect1}>
+                <Option value="1">自高到低</Option>
+                <Option value="2">自低到高</Option>
+              </Select>
+            </div>
+            <div className={styles.iconBtn}>
+              <Icon type="tables" className={styles.fixMargin} />
+              <Icon type="zhuzhuangtu" className={styles.fixMargin} />
+            </div>
+            <div className={styles.iconBtn}>
+              <Icon type="daochu" />
+            </div>
           </div>
         </div>
         <ChartBoard chartData={chartData} />
