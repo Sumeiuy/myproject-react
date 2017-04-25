098ab6d4170230d04fc83ed4b34cd6bf1c26f277
unknown <>
2017-04-25 10:39:45 +0800

    Liujianshu:修改新 UI

--- src/components/invest/PerformanceItem.js
+++ src/components/invest/PerformanceItem.js
@@ -19,16 +19,25 @@ export default class PerformanceItem extends PureComponent {
 
   render() {
     const { data } = this.props;
+    // 数据的长度
+    const length = data.length;
+    // 数据的行数
+    const lines = Math.ceil(length / 8);
+    // 一行时无边框
+    const borderName = lines === 1 ? styles.noBorder : '';
+    // 最后一行开始的索引
+    const lastLineFirst = ((lines - 1) * 8) - 1;
     return (
       <div>
         <div className={styles.titleText}>总量指标</div>
-        <div className={styles.items}>
-          <Row gutter={4}>
+        <div className={`${borderName} ${styles.items}`}>
+          <Row>
             {
-              data && data.map((item, index) => {
+              data.map((item, index) => {
                 const uniqueKey = `invest-index-${index}`;
+                const itemBorder = index > lastLineFirst ? styles.noBorder : '';
                 return (
-                  <Col key={uniqueKey} span={3} className={styles.itemWrap}>
+                  <Col key={uniqueKey} span={3} className={`${itemBorder} ${styles.itemWrap}`}>
                     <Item data={item} />
                   </Col>
                 );
