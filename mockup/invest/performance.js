78c9963ea451bdcb6fcbb23fb5df4bad9722b357
3610cn <maoquan@htsc.com>
2017-05-02 16:25:42 +0800

    解决fetch默认不带cookie的问题

--- mockup/invest/performance.js
+++ mockup/invest/performance.js
@@ -1,7 +1,7 @@
 // 使用 Mock
 var Mock = require('mockjs');
 let data = Mock.mock({
-    'performance|1-20':[{
+    'performance|8-20':[{
     'title|1': ['投顾入岗人数','签约客户数','签约资金','签约平均佣金率','投顾净佣金收入','资产配置覆盖率','托管总资产','MOT任务完成率'],
     'num|500-1000': 1,
     'unit|1': ['户', '亿', '%', '万'],
