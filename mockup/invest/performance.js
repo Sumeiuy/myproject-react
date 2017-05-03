f5c13c4abae14a71c15de4b4cf91695fd3a20911
unknown <ilinmu@qq.com>
2017-05-03 10:03:39 +0800

    刘建树：解决冲突

--- mockup/invest/performance.js
+++ mockup/invest/performance.js
@@ -1,15 +1,4 @@
 // 使用 Mock
-<<<<<<< Updated upstream
-var Mock = require('mockjs');
-let data = Mock.mock({
-    'performance|8-20':[{
-    'title|1': ['投顾入岗人数','签约客户数','签约资金','签约平均佣金率','投顾净佣金收入','资产配置覆盖率','托管总资产','MOT任务完成率'],
-    'num|500-1000': 1,
-    'unit|1': ['户', '亿', '%', '万'],
-    'icon|1': ['ren', 'kh', 'zc', 'tg','per'],
-  }]
-})
-=======
 // var Mock = require('mockjs');
 // let data = Mock.mock({
 //     'performance|1-20':[{
@@ -19,7 +8,7 @@ let data = Mock.mock({
 //     'icon|1': ['ren', 'kh', 'zc', 'tg','per'],
 //   }]
 // })
->>>>>>> Stashed changes
+
 exports.response = function (req, res) {
   return {
     "code": "0",
