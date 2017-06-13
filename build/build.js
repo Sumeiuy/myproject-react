634eeb11a204a4e1962fecfd91c0c8d16d17a629
3610cn <maoquan@htsc.com>
2017-06-13 13:25:57 +0800

    构建脚本生成assets引用文件

--- build/build.js
+++ build/build.js
@@ -7,8 +7,12 @@ var rm = require('rimraf')
 var path = require('path')
 var chalk = require('chalk')
 var webpack = require('webpack')
+var _ = require('lodash');
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+
 var config = require('../config')
 var webpackConfig = require('./webpack.prod.conf')
+var assetsGenerator = require('./assets-generator')
 
 var spinner = ora('building for production...')
 spinner.start()
@@ -31,5 +35,13 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
       '  Tip: built files are meant to be served over an HTTP server.\n' +
       '  Opening index.html over file:// won\'t work.\n'
     ))
+    
+    console.log('generate assets ref file...')
+    var htmlWebpackPlugin = _.find(
+      webpackConfig.plugins,
+      plugin => plugin instanceof HtmlWebpackPlugin
+    );
+    assetsGenerator(JSON.parse(htmlWebpackPlugin.assetJson));
+    console.log(chalk.cyan('generate assets ref complete.'))
   })
 })
