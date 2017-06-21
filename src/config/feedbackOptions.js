8839fbf5cdcfde00a2001a1e7212fff419bd50b5
hongguangqing <1757912135@qq.com>
2017-06-21 16:39:32 +0800

    按照接口字段修改参数

--- src/config/feedbackOptions.js
+++ src/config/feedbackOptions.js
@@ -29,48 +29,48 @@ const feedbackOptions = {
         },
       ],
     },
-    {
-      value: 'FSP',
-      label: 'FSP',
-      children: [
-        {
-          value: 'jxst',
-          label: '绩效视图',
-        },
-        {
-          value: 'khzx',
-          label: '客户中心',
-        },
-        {
-          value: 'zwzx',
-          label: '任务中心',
-        },
-        {
-          value: 'cpxs',
-          label: '产品销售',
-        },
-        {
-          value: 'zcpz',
-          label: '资产配置',
-        },
-        {
-          value: 'tjcx',
-          label: '统计查询',
-        },
-        {
-          value: 'zxzx',
-          label: '资讯中心',
-        },
-        {
-          value: 'zsk',
-          label: '知识库',
-        },
-        {
-          value: 'ywsq',
-          label: '业务申请',
-        },
-      ],
-    },
+    // {
+    //   value: 'FSP',
+    //   label: 'FSP',
+    //   children: [
+    //     {
+    //       value: 'jxst',
+    //       label: '绩效视图',
+    //     },
+    //     {
+    //       value: 'khzx',
+    //       label: '客户中心',
+    //     },
+    //     {
+    //       value: 'zwzx',
+    //       label: '任务中心',
+    //     },
+    //     {
+    //       value: 'cpxs',
+    //       label: '产品销售',
+    //     },
+    //     {
+    //       value: 'zcpz',
+    //       label: '资产配置',
+    //     },
+    //     {
+    //       value: 'tjcx',
+    //       label: '统计查询',
+    //     },
+    //     {
+    //       value: 'zxzx',
+    //       label: '资讯中心',
+    //     },
+    //     {
+    //       value: 'zsk',
+    //       label: '知识库',
+    //     },
+    //     {
+    //       value: 'ywsq',
+    //       label: '业务申请',
+    //     },
+    //   ],
+    // },
   ],
   typeOptions: [
     {
