60d8881e74f667dc7128d98bb0151a518952ee9f
武汉佰钧成-洪光情 <k0180004@test.htsc.com.cn>
2017-07-26 19:28:06 +0800

    修改样式

--- src/components/pageCommon/PageAnchor.js
+++ src/components/pageCommon/PageAnchor.js
@@ -54,7 +54,7 @@ export default class PageAnchor extends PureComponent {
       <div>
         {
           charInfoLength >= 3 ?
-            <Anchor className={styles.pageAnchor} offsetTop={115} >
+            <Anchor className={styles.pageAnchor} offsetTop={130} >
               {
                 chartInfo.map((item) => {
                   const { key } = item;
