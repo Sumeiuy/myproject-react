5244180907f2f61f2c7b8191f426669f04bdb13b
yangquanjian <522564726@qq.com>
2017-08-07 15:07:02 +0800

    获取用户职位信息

--- src/models/app.js
+++ src/models/app.js
@@ -3,11 +3,32 @@
  *  全局模型管理
  * @author maoquan(maoquan@htsc.com)
  */
+import api from '../api';
 
 export default {
   namespace: 'app',
-  state: {},
+  state: {
+    empInfo: {},
+  },
   subscriptions: {},
-  effects: {},
-  reducers: {},
+  effects: {
+    // 获取员工职责与职位
+    * getEmpInfo({ payload }, { call, put }) {
+      const resultData = yield call(api.getEmpInfo);
+      yield put({
+        type: 'getEmpInfoSuccess',
+        response: resultData,
+      });
+    },
+  },
+  reducers: {
+    // 获取员工职责与职位
+    getEmpInfoSuccess(state, action) {
+      const { response: { resultData } } = action;
+      return {
+        ...state,
+        empInfo: resultData,
+      };
+    },
+  },
 };
