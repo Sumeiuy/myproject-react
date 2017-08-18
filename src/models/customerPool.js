b3f6e85faf2636734b7d050d554b903f9acfe1b3
yangquanjian <522564726@qq.com>
2017-08-18 09:01:16 +0800

    #FSP-5385#组织结构树请求暂时使用之前的

--- src/models/customerPool.js
+++ src/models/customerPool.js
@@ -48,7 +48,7 @@ export default {
     },
     // 获取客户范围
     * getCustomerScope({ payload }, { call, put }) {
-      const resultData = yield call(api.getCustRangeAll);
+      const resultData = yield call(api.getCustRange);
       yield put({
         type: 'getCustomerScopeSuccess',
         payload: resultData,
