dcb129352f1e1b158ddde334b3638c5f754cce7a
K0240010 <xuxiaoqin@test.com.cn>
2017-09-04 12:16:08 +0800

    #FSP-0000#修改客户池首页bug

--- src/components/customerPool/home/ToBeDone.js
+++ src/components/customerPool/home/ToBeDone.js
@@ -120,7 +120,7 @@ export default class PerformanceIndicators extends PureComponent {
             </Col>
             <Col span={6}>
               <div className={`${styles.item} ${styles.item_d}`}>
-                <a className="item" onClick={() => fspGlobal.openRctTab({ notificationUrl, notificationParam })}>
+                <a className="item" onClick={() => fspGlobal.openRctTab({ notificationUrl, param: notificationParam })}>
                   <div className={styles.content}>
                     <h1>{this.processNum(notificationNumbers)}</h1>
                     <p>消息提醒</p>
