40aeedf29fda5ff16f9f5d7b3d7d4171d4c46260
K0170095 <baojiajia@git.com.cn>
2017-09-13 15:25:57 +0800

    #FSP-5788#佣金调整详情界面样式数据写死

--- src/components/commissionAdjustment/Detail.js
+++ src/components/commissionAdjustment/Detail.js
@@ -6,47 +6,39 @@
 
 import React, { PureComponent } from 'react';
 import { Row, Col } from 'antd';
-import './detail.less';
+import classnames from 'classnames';
+import styles from './detail.less';
 
 export default class Commissiondetail extends PureComponent {
   render() {
+     const leftItem = classnames({
+      [styles.itemleft]: true,
+      [styles.item]: true,
+    });
+     const rightItem = classnames({
+      [styles.itemright]: true,
+      [styles.item]: true,
+    });
     return (
-      <div className="detail_box">
-        <div className="inner">
-          <h1 className="bugtitle">编号11222</h1>
-          <div className="row_box">
-            <Row>
-              <Col span="24">
-                <div id="detail_module" className="module">
-                  <div className="mod_header">
-                    <h2 className="toogle_title">基本信息</h2>
+      <div className={styles.detail_box}>
+        <div className={styles.inner}>
+          <h1 className={styles.bugtitle}>编号11222</h1>
+           <div id="detail_module" className={styles.module}>
+                  <div className={styles.mod_header}>
+                    <h2 className={styles.toogle_title}>基本信息</h2>
                   </div>
-                  <div className="mod_content">
-                    <ul className="property_list clearfix">
-                      <li className="item">
-                        <div className="wrap value_word">
-                          <strong className="name">标题：</strong>
-                          <span className="value">标题要长长长长长长</span>
+                  <div className={styles.mod_content}>
+                    <ul className={styles.property_list}>
+                      <li className={styles.item}>
+                        <div className={styles.wrap}>
+                          <span className={styles.itemname}>子类型：</span>
+                          <span className={styles.itemvalue}>私密客户权限分配</span>
                         </div>
                       </li>
-                      <li className="item">
-                        <div className="wrap">
-                          <strong className="name">子类型：</strong>
-                          <span className="value">私密客户权限分配</span>
-                        </div>
-                      </li>
-                      <li className="item">
-                        <div className="wrap value_word">
-                          <strong className="name">客户：</strong>
-                          <span className="value">
-                            张三 123456
-                          </span>
-                        </div>
-                      </li>
-                      <li className="item">
-                        <div className="wrap value_word">
-                          <strong className="name">备注：</strong>
-                          <span className="value">
+                      <li className={styles.item}>
+                        <div className={styles.wrap}>
+                          <span className={styles.itemname}>备注：</span>
+                          <span className={styles.itemvalue}>
                             这里是备注备注备注
                           </span>
                         </div>
@@ -54,31 +46,28 @@ export default class Commissiondetail extends PureComponent {
                     </ul>
                   </div>
                 </div>
-              </Col>
-            </Row>
-          </div>
-          <div id="nginformation_module" className="module">
-            <div className="mod_header">
-              <h2 className="toogle_title">拟稿信息</h2>
+          <div id="nginformation_module" className={styles.module}>
+            <div className={styles.mod_header}>
+              <h2 className={styles.toogle_title}>拟稿信息</h2>
             </div>
-            <div className="mod_content">
-              <ul className="property_list clearfix">
-                <li className="item">
-                  <div className="wrap value_word">
-                    <strong className="name">拟稿人：</strong>
-                    <span className="value">南京分公司长江路营业部-李四（0016533333）</span>
+            <div className={styles.mod_content}>
+              <ul className={styles.property_list}>
+                <li className={styles.item}>
+                  <div className={styles.wrap}>
+                    <span className={styles.itemname}>拟稿人：</span>
+                    <span className={styles.itemvalue}>南京分公司长江路营业部-李四（0016533333）</span>
                   </div>
                 </li>
-                <li className="item">
-                  <div className="wrap">
-                    <strong className="name">提请时间：</strong>
-                    <span className="value">2017/08/31</span>
+                <li className={styles.item}>
+                  <div className={styles.wrap}>
+                    <span className={styles.itemname}>提请时间：</span>
+                    <span className={styles.itemvalue}>2017/08/31</span>
                   </div>
                 </li>
-                <li className="item">
-                  <div className="wrap value_word">
-                    <strong className="name">状态：</strong>
-                    <span className="value">
+                <li className={styles.item}>
+                  <div className={styles.wrap}>
+                    <span className={styles.itemname}>状态：</span>
+                    <span className={styles.itemvalue}>
                      已完成
                     </span>
                   </div>
@@ -89,61 +78,63 @@ export default class Commissiondetail extends PureComponent {
               </div> */ }
             </div>
           </div>
-          <div id="customer_module" className="module">
-            <div className="mod_header">
-              <h2 className="toogle_title">客户信息：</h2>
+          <div id="customer_module" className={styles.module}>
+            <div className={styles.mod_header}>
+              <h2 className={styles.toogle_title}>客户信息：</h2>
             </div>
-            <div className="mod_content">
+            <div className={styles.mod_content}>
               客户列表
             </div>
           </div>
-          <div id="choosecommission" className="module">
-            <div className="mod_header">
-              <h2 className="toogle_title">佣金产品选择：</h2>
+          <div id="choosecommission" className={styles.module}>
+            <div className={styles.mod_header}>
+              <h2 className={styles.toogle_title}>佣金产品选择：</h2>
             </div>
-            <div className="mod_content">
-              <li className="item">
-                <div className="wrap">
-                  <strong className="name">目标佣金率（股基）：</strong>
-                  <span className="value">25‰</span>
+            <div className={styles.mod_content}>
+            <ul className={styles.property_list}>
+              <li className={styles.item}>
+                <div className={styles.wrap}>
+                  <span className={styles.itemname}>目标佣金率（股基）：</span>
+                  <span className={styles.itemvalue}>25‰</span>
                 </div>
               </li>
-              <li className="item">
-                <div className="wrap">
-                  <strong className="name">目标产品：</strong>
-                  <span className="value">PPKD02/。。。。。</span>
+              <li className={styles.item}>
+                <div className={styles.wrap}>
+                  <span className={styles.itemname}>目标产品：</span>
+                  <span className={styles.itemvalue}>PPKD02/。。。。。</span>
                 </div>
               </li>
+              </ul>
             </div>
           </div>
-          <div id="processing" className="module">
-            <div className="mod_header">
-              <h2 className="toogle_title">其他佣金费率：</h2>
+          <div id="processing" className={styles.module}>
+            <div className={styles.mod_header}>
+              <h2 className={styles.toogle_title}>其他佣金费率：</h2>
             </div>
-            <div className="mod_content">
-              <ul className="property_list clearfix">
-                <li className="item">
-                  <div className="wrap value_word">
-                    <strong className="name">B股：</strong>
-                    <span className="value">内容</span>
+            <div className={styles.mod_content}>
+              <ul className={styles.property_list}>
+                <li className={leftItem}>
+                  <div className={styles.wrap}>
+                    <span className={styles.itemname}>B股：</span>
+                    <span className={styles.itemvalue}>内容</span>
                   </div>
                 </li>
-                <li className="item item-right">
-                  <div className="wrap value_word">
-                    <strong className="name">担保权证</strong>
-                    <span className="value tiem-orient">内容</span>
+                <li className={rightItem}>
+                  <div className={styles.wrap}>
+                    <span className={styles.itemname}>担保权证</span>
+                    <span className={styles.itemvalue}>内容</span>
                   </div>
                 </li>
-                <li className="item">
-                  <div className="wrap value_word">
-                    <strong className="name">债券：</strong>
-                    <span className="value">内容</span>
+                <li className={leftItem}>
+                  <div className={styles.wrap}>
+                    <span className={styles.itemname}>债券：</span>
+                    <span className={styles.itemvalue}>内容</span>
                   </div>
                 </li>
-                <li className="item item-right">
-                  <div className="wrap value_word">
-                    <strong className="name">信用顾基：</strong>
-                    <span className="value tiem-orient">内容</span>
+                <li className={rightItem}>
+                  <div className={styles.wrap}>
+                    <span className={styles.itemname}>信用顾基：</span>
+                    <span className={styles.itemvalue}>内容</span>
                   </div>
                 </li>
               </ul>
