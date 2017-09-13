304cdc94abb3e641abb816ff6fd9f74e81205956
K0180004 <hongguangqing34615@billjc.com>
2017-09-13 13:26:24 +0800

    #FSP-5691#权限申请列表优化

--- src/routes/permission/Home.js
+++ src/routes/permission/Home.js
@@ -10,21 +10,17 @@ import { autobind } from 'core-decorators';
 import _ from 'lodash';
 import { connect } from 'react-redux';
 import { withRouter, routerRedux } from 'dva/router';
-import classnames from 'classnames';
-import Icon from '../../components/common/Icon';
 import { constructPermissionPostBody } from '../../utils/helper';
 import SplitPanel from '../../components/common/splitPanel/SplitPanel';
 import PageHeader from '../../components/permission/PageHeader';
 import PermissionList from '../../components/common/biz/CommonList';
-import { permissionOptions } from '../../config';
+import seibelColumns from '../../components/common/biz/seibelColumns';
 
 import styles from './home.less';
 
 const EMPTY_LIST = [];
 const EMPTY_OBJECT = {};
 const OMIT_ARRAY = ['currentId', 'isResetPageNum'];
-// 状态字典
-const STATUS_MAP = permissionOptions.stateOptions;
 const mapStateToProps = state => ({
   list: state.permission.list,
 });
@@ -130,56 +126,16 @@ export default class Permission extends PureComponent {
 
   /**
    * 构造表格的列数据
+   * 传参为icon的type
    */
   @autobind
   constructTableColumns() {
-    const columns = [{
-      dataIndex: 'serialNumber.subType.title.empName.empId',
-      width: '60%',
-      render: (text, record) => (
-        <div className="leftSection">
-          <div className="id">
-            <Icon type="save_blue" />
-            <span className="serialNumber">编号{record.serialNumber || '无'}</span>
-            <span className="subType">{record.subType || '无'}</span>
-          </div>
-          <div className="title">{record.title || '无'}</div>
-          <div className="address">拟稿人：{record.empName}({record.empId})，{`${record.level2OrgName || ''}${record.level3OrgName || ''}` || '无'}</div>
-        </div>
-        ),
-    }, {
-      dataIndex: 'status.createTime.custName.custNumber',
-      width: '40%',
-      render: (text, record) => {
-        // 当前行记录
-        let statusClass;
-        let statusLabel;
-        if (record.status) {
-          statusClass = classnames({
-            'state-complete': record.status === STATUS_MAP[0].value,
-            'state-resolve': record.status === STATUS_MAP[1].value,
-            'state-close': record.status === STATUS_MAP[2].value,
-          });
-          statusLabel = STATUS_MAP.filter(item => item.value === record.status);
-        }
-        return (
-          <div className="rightSection">
-            <div className={statusClass}>{(!_.isEmpty(statusLabel) && statusLabel[0].label) || '无'}</div>
-            <div className="date">{(record.createTime &&
-              record.createTime.slice(0, 10)) || '无'}</div>
-            <div className="cust">客户:{record.custName || '无'}({record.custNumber || '无'})</div>
-          </div>
-        );
-      },
-    }];
-
-    return columns;
+    return seibelColumns('save_blue');
   }
 
   render() {
     const { list, location, replace } = this.props;
     const { isEmpty } = this.state;
-    const columns = this.constructTableColumns();
     const topPanel = (
       <PageHeader
         location={location}
@@ -191,7 +147,7 @@ export default class Permission extends PureComponent {
         list={list}
         replace={replace}
         location={location}
-        columns={columns}
+        columns={this.constructTableColumns()}
       />
     );
 
