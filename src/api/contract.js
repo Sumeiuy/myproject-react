27f861d6edcf51deb3a5d021d397caeaecbbb582
K0240007 <jianshu.liu@cienet.com.cn>
2017-09-18 17:46:34 +0800

    #FSP-5796#合作合约列表布局

--- src/api/contract.js
+++ src/api/contract.js
@@ -5,5 +5,9 @@
 export default function contract(api) {
   return {
     getContractDetail: query => api.post('/groovynoauth/fsp/contract/cooper/queryDetail', query),
+    // 获取详情信息
+    getMessage: query => api.post('/groovynoauth/contract/contractMessage', query),
+    // 获取权限申请列表
+    getContractList: query => api.post('/groovynoauth/contract/queryContractList', query),
   };
 }
