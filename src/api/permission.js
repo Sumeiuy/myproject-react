b4d637dab66ed70481dbb3acb31582ed997dd71a
K0180004 <hongguangqing34615@billjc.com>
2017-09-19 13:47:02 +0800

    #FSP-6005#修改权限申请的mockup

--- src/api/permission.js
+++ src/api/permission.js
@@ -5,9 +5,9 @@
 export default function permission(api) {
   return {
     // 获取详情信息
-    getMessage: query => api.post('/groovynoauth/smkh/permissionMessage', query),
+    getMessage: query => api.post('/groovynoauth/smkh/queryApplicationDetail', query),
     // 获取权限申请列表
-    getPermissionList: query => api.post('/groovynoauth/smkh/queryPermissionList', query),
+    getPermissionList: query => api.post('/groovynoauth/smkh/queryApplicationList', query),
     // 获取拟稿人
     getDrafterList: query => api.post('/groovynoauth/smkh/queryEmpList', query),
     // 获取部门
