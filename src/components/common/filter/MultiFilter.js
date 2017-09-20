2912622ca87547c02c4c4966b120af38cedac157
K0240008 <junjun.wang@cienet.com.cn>
2017-09-20 18:21:32 +0800

    #FSP-5633#全渠道服务记录筛选功能开发和bug修改

--- src/components/common/filter/MultiFilter.js
+++ src/components/common/filter/MultiFilter.js
@@ -1,7 +1,15 @@
 /**
- * @file components/customerPool/SingleFilter.js
- *  客户池-客户列表单项筛选
+ * @file components/common/filter/SingleFilter.js
+ *  多项筛选
  * @author wangjunjun
+ *
+ * filterLabel string类型 筛选的对象 eg: '客户类型'
+ * filterField array类型 筛选项
+ * filter string类型 onChange回调方法中返回的对象的name值
+ * value string类型 回填到组件的值，也是onChange回调方法中返回的对象的value值
+ * separator string类型 多选的分割符号，可选, 默认逗号分割
+ * onChange function类型 组件的回调方法，获取已选中的值
+ *             返回一个对象 { name: 'name', value: 'value' }
  */
 
 import React, { PropTypes, PureComponent } from 'react';
@@ -10,11 +18,13 @@ import _ from 'lodash';
 
 import styles from './filter.less';
 
+const currentClass = 'current';
+
 const generateCls = (v, k) => {
-  if (v === '' && v === k) {
-    return 'current';
-  } else if (v.split(',').indexOf(k) > -1) {
-    return 'current';
+  if (_.isEmpty(v) && k === '') {
+    return currentClass;
+  } else if (v.indexOf(k) > -1) {
+    return currentClass;
   }
   return '';
 };
@@ -26,30 +36,33 @@ export default class MultiFilter extends PureComponent {
     filterField: PropTypes.array,
     onChange: PropTypes.func.isRequired,
     value: PropTypes.string.isRequired,
+    separator: PropTypes.string,
   }
 
   static defaultProps = {
     filterField: [],
+    separator: ',',
   }
 
   constructor(props) {
     super(props);
+    const { value, separator } = props;
     this.state = {
-      keyArr: props.value ? props.value.split(',') : [],
+      keyArr: value ? value.split(separator) : [],
     };
   }
 
   @autobind
   handleClick(value) {
     const { keyArr } = this.state;
-    const { filter, onChange } = this.props;
+    const { separator, filter, onChange } = this.props;
     if (value) {
       this.setState({
         keyArr: _.includes(keyArr, value) ? keyArr.filter(v => v !== value) : [...keyArr, value],
       }, () => {
         onChange({
           name: filter,
-          value: this.state.keyArr.join(','),
+          value: this.state.keyArr.join(separator),
         });
       });
     } else {
@@ -66,11 +79,12 @@ export default class MultiFilter extends PureComponent {
 
   @autobind
   renderList() {
-    const { filterField, value } = this.props;
+    const { filterField } = this.props;
+    const { keyArr } = this.state;
     return filterField.map(item => (
       <li
         key={item.key}
-        className={generateCls(value, item.key)}
+        className={generateCls(keyArr, item.key)}
         onClick={() => this.handleClick(item.key)}
       >
         {item.value}
---
+++ src/models/fullChannelServiceRecord.js
@@ -0,0 +1,17 @@
+/**
+ * @file models/fullChannelServiceRecord.js
+ *  全渠道服务记录管理
+ * @author wagnjunjun
+ */
+// import { fullChannelServiceRecord as api } from '../api';
+
+export default {
+  namespace: 'fullChannelServiceRecord',
+  state: {
+  },
+  subscriptions: {},
+  effects: {
+  },
+  reducers: {
+  },
+};
