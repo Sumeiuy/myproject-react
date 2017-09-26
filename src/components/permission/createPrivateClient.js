55c6206f83acd5308bbed7807a40dc4f4b06ba21
K0240010 <xuxiaoqin@test.com.cn>
2017-09-26 16:47:22 +0800

    #FSP-6059#客户搜索接口联调，修复bug

--- src/components/permission/createPrivateClient.js
+++ src/components/permission/createPrivateClient.js
@@ -44,6 +44,7 @@ export default class CreatePrivateClient extends PureComponent {
   @autobind
   onOk() {
     console.log('确定要关闭吗');
+    // this.setState({isShowModal: false});
   }
 
   @autobind
