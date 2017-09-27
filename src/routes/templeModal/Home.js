52a1d28e54c3a3787b6b4cfb41a5b35142f2c52b
K0100010 <k0100010@test.htsc.com.cn>
2017-09-27 13:47:32 +0800

    #FSP-6292#根据需求，修改穿梭框的样式和逻辑

--- src/routes/templeModal/Home.js
+++ src/routes/templeModal/Home.js
@@ -20,7 +20,7 @@ import InfoItem from '../../components/common/infoItem';
 import SearchSelect from '../../components/common/Select/SearchSelect';
 import DigitalTrimmer from '../../components/common/DigitalTrimmer';
 import ApprovalRecordBoard from '../../components/commissionAdjustment/ApprovalRecordBoard';
-import OperationOfCustermorList from '../../components/commissionAdjustment/OperationOfCustermorList'
+import OperationOfCustermorList from '../../components/commissionAdjustment/OperationOfCustermorList';
 
 import {
   confirmData,
@@ -228,11 +228,13 @@ export default class TemplModal extends PureComponent {
     };
 
     const transferProps = {
-      subscribeData: subscribelData,
-      unsubscribeData: unsubcribeData,
-      subscribeColumns: productColumns,
-      unsubscribeColumns: productColumns,
+      firstData: subscribelData,
+      secondData: unsubcribeData,
+      firstColumns: productColumns,
+      secondColumns: productColumns,
       onChange: this.handleChange,
+      onSearch: this.handleSearch,
+      rowKey: 'key',
     };
 
 
