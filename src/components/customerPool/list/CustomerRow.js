52c6d73c7d165a1a92c7c0736f52542f776254cd
K0240008 <junjun.wang@cienet.com.cn>
2017-10-09 13:33:06 +0800

    #FSP-6265#客户列表客户性质字段修改2

--- src/components/customerPool/list/CustomerRow.js
+++ src/components/customerPool/list/CustomerRow.js
@@ -178,8 +178,8 @@ export default class CustomerRow extends PureComponent {
     const { listItem } = this.props;
     if (listItem.pOrO === 'P') {
       return <span>{listItem.genderValue}/{listItem.age}岁</span>;
-    } else if (listItem.pOrO === 'O' && listItem.orgTypeCode) {
-      return <span>{listItem.orgTypeCode}</span>;
+    } else if (listItem.pOrO === 'O' && listItem.orgTypeName) {
+      return <span>{listItem.orgTypeName}</span>;
     } else if (listItem.pOrO === 'F' && listItem.prodTypeCode) {
       return <span>{listItem.prodTypeCode}</span>;
     }
