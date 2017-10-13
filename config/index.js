b7627f33c8b67b2b87746d022dac403ba723967f
K0180004 <hongguangqing34615@billjc.com>
2017-10-13 14:56:21 +0800

    #FSP-6551#修改权限申请当前审批人

--- config/index.js
+++ config/index.js
@@ -31,8 +31,8 @@ module.exports = {
     proxyTable: {
       '/fspa/mcrm/api': {
 
-        target: 'http://168.61.8.81:5085', // SIT
-        // target: 'http://168.61.8.82:5086', // UAT
+        // target: 'http://168.61.8.81:5085', // SIT
+        target: 'http://168.61.8.82:5086', // UAT
         secure: false,
       },
       '/screenshot': {
@@ -49,7 +49,7 @@ module.exports = {
     // In our experience, they generally work as expected,
     // just be aware of this issue when enabling this option.
     cssSourceMap: false,
-    mock: true,
+    mock: false,
   },
   cssModules: true,
   appSrc: path.resolve(__dirname, '../src'),
