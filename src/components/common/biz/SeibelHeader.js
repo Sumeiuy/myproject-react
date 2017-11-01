ecf6fc3314cec8c9b6ade77924f82612fd036daa
K0180004 <hongguangqing34615@billjc.com>
2017-11-01 20:42:33 +0800

    #FSP-6745#修改样式

--- src/components/common/biz/SeibelHeader.js
+++ src/components/common/biz/SeibelHeader.js
@@ -99,7 +99,6 @@ export default class Pageheader extends PureComponent {
   @autobind
   onWindowResize() {
     const filterBoxHeight = this.filterBox.getBoundingClientRect().height;
-    console.warn('filterBoxHeight', filterBoxHeight);
     if (filterBoxHeight <= FILTERBOX_HEIGHT) {
       removeClass(this.filterMore, 'filterMoreIcon');
       addClass(this.filterMore, 'filterNoneIcon');
