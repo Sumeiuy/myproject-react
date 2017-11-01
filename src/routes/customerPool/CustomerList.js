932f2357eb9783d415dc73c44fa825c7ab2e3f1e
K0240008 <junjun.wang@cienet.com.cn>
2017-11-01 16:11:58 +0800

    #FSP-7232#update

--- src/routes/customerPool/CustomerList.js
+++ src/routes/customerPool/CustomerList.js
@@ -568,7 +568,6 @@ export default class CustomerList extends PureComponent {
       cycleTimeProps.cycle = cycle;
       cycleTimeProps.selectValue = selectValue;
     }
-    // console.log('routes>>>>>>>>>>>>>>>>>>>>>>>>', this.props.routes);
     return (
       <div className={styles.customerlist}>
         <Row type="flex" justify="space-between" align="middle">
