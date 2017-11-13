4ea57a4cb4629fce334075055de96ac918ccb660
K0240008 <junjun.wang@cienet.com.cn>
2017-11-13 16:19:14 +0800

    #FSP-7535#修改客户列表页服务经理组件

--- src/components/common/dropdownSelect/index.js
+++ src/components/common/dropdownSelect/index.js
@@ -4,11 +4,13 @@
  */
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import { Icon } from 'antd';
+import { Input } from 'antd';
 import { autobind } from 'core-decorators';
 import classnames from 'classnames';
 import _ from 'lodash';
 import style from './style.less';
+
+const Search = Input.Search;
 
 export default class DropdownSelect extends PureComponent {
   static propTypes = {
@@ -117,9 +119,12 @@ export default class DropdownSelect extends PureComponent {
   }
 
   @autobind
-  toSearch() {
+  toSearch(value) {
     // 在这里去触发查询搜索信息的方法
-    this.props.emitToSearch(this.state.searchValue);
+    this.props.emitToSearch(value);
+    this.setState({
+      value,
+    });
   }
 
   @autobind
@@ -176,19 +181,11 @@ export default class DropdownSelect extends PureComponent {
             className={style.ddsDrapMenuSearch}
             onClick={(e) => { e.nativeEvent.stopImmediatePropagation(); }}
           >
-            <input
-              type="text"
+            <Search
               className={style.searhInput}
-              value={this.state.searchValue}
               placeholder={this.props.placeholder}
-              onChange={(e) => { this.setState({ searchValue: e.target.value }); }}
+              onSearch={this.toSearch}
             />
-            <span
-              className={style.searchSub}
-              onClick={this.toSearch}
-            >
-              <Icon type="search" />
-            </span>
           </div>
           {
             _.isEmpty(this.props.searchList)
