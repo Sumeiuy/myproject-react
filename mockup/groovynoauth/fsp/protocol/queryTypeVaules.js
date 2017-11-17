b88575e474b2c37b7b1ea863e4f48fe5950231a7
K0050045 <xuwenkang@chinasoft.com>
2017-11-17 14:53:38 +0800

    #FSP-7400# 通道类型协议Bug修改

--- mockup/groovynoauth/fsp/protocol/queryTypeVaules.js
+++ mockup/groovynoauth/fsp/protocol/queryTypeVaules.js
@@ -4,18 +4,11 @@ exports.response = function (req, res) {
     "msg": "OK",
     "resultData": [
       {
-        "approvalFlg": "N",
-        "invstCatgCd": "507070",
-        "orderableFlg": "N",
-        "packageFlg": "N",
-        "prodCd": "Promotion",
-        "prodId": "zjkcd013",
-        "prodName": "紫金快车道全赢版",
-        "promoTypeCd": "市场活动协议",
-        "rowId": "1-41ZYSFB",
-        "serviceLength": "24",
-        "statusCd": "415010",
-        "subTypeCd": "502010",
+        "val": "紫金快车道协议",
+        "name": "507070",
+        "faName": "502010",
+        "type": "FINS_INVST_CATEGORY",
+        "rowId": "1-3PX34LH"
       }
     ]
   }
