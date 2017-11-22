3d99778b93f5198c1ca5c4e0135ab2cbed3b61c2
K0240010 <xuxiaoqin@test.com.cn>
2017-11-22 20:10:08 +0800

    #FSP-000#执行者视图服务记录组建开发

--- config/index.js
+++ config/index.js
@@ -24,7 +24,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: 9083,
+    port: 9088,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
--- src/routes/customerPool/PerformerView.js
+++ src/routes/customerPool/PerformerView.js
@@ -15,40 +15,65 @@ import ConnectedSeibelHeader from '../../components/common/biz/ConnectedSeibelHe
 import SplitPanel from '../../components/common/splitPanel/CutScreen';
 import PerformerViewList from '../../components/common/appList';
 import PerformerViewDetail from '../../components/customerPool/performerView/PerformerViewDetail';
+import AppItem from '../../components/common/appList/AppItem';
 import { seibelConfig } from '../../config';
+import appListTool from '../../components/common/appList/tool';
 
 const { performerView, performerView: { pageType, subType, status } } = seibelConfig;
+
 const fetchDataFunction = (globalLoading, type) => query => ({
   type,
   payload: query || {},
   loading: globalLoading,
 });
+
+const effects = {
+  getSeibleList: 'app/getSeibleList',
+  addServeRecord: 'customerPool/addServeRecord',
+};
+
 const mapStateToProps = state => ({
   // 左侧列表数据
-  performerViewList: state.app.seibleList,
+  list: state.app.seibleList,
+  dict: state.app.dict,
 });
+
 const mapDispatchToProps = {
   replace: routerRedux.replace,
   // 获取左侧列表
-  getPerformerViewList: fetchDataFunction(true, 'app/getSeibleList'),
+  getPerformerViewList: fetchDataFunction(true, effects.getSeibleList),
+  // 添加服务记录
+  addServeRecord: fetchDataFunction(true, effects.addServeRecord),
 };
+
 @connect(mapStateToProps, mapDispatchToProps)
 @withRouter
 export default class PerformerView extends PureComponent {
   static propTypes = {
     location: PropTypes.object.isRequired,
     replace: PropTypes.func.isRequired,
-    performerViewList: PropTypes.object.isRequired,
+    list: PropTypes.object.isRequired,
     getPerformerViewList: PropTypes.func.isRequired,
+    addServeRecord: PropTypes.func.isRequired,
+    dict: PropTypes.object.isRequired,
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentSubtype: '',
+      isEmpty: true,
+      activeRowIndex: 0,
+    };
   }
 
   componentWillMount() {
     const {
       location: {
         query,
-        query: {
+      query: {
           pageNum,
-          pageSize,
+        pageSize,
         },
       },
       getPerformerViewList,
@@ -67,14 +92,54 @@ export default class PerformerView extends PureComponent {
 
   }
 
+  // 点击列表每条的时候对应请求详情
+  @autobind
+  handleListRowClick(record, index) {
+    const { id, subType: st } = record;
+    const {
+      replace,
+      location: { pathname, query, query: { currentId } },
+    } = this.props;
+    if (currentId === id) return;
+    replace({
+      pathname,
+      query: {
+        ...query,
+        currentId: id,
+      },
+    });
+    this.setState({ currentSubtype: st, activeRowIndex: index });
+    // this.getDetail4Subtye(record);
+  }
+
+  // 渲染列表项里面的每一项
+  @autobind
+  renderListRow(record, index) {
+    const { activeRowIndex } = this.state;
+    return (
+      <AppItem
+        key={record.id}
+        data={record}
+        active={index === activeRowIndex}
+        onClick={this.handleListRowClick}
+        index={index}
+        pageName="performerView"
+        type="yongjin"
+        pageData={performerView}
+      />
+    );
+  }
+
   render() {
     const {
       location,
       replace,
-      performerViewList,
+      list,
+      dict,
+      addServeRecord,
     } = this.props;
 
-    const isEmpty = _.isEmpty(performerViewList.resultData);
+    const isEmpty = _.isEmpty(list.resultData);
     const topPanel = (
       <ConnectedSeibelHeader
         location={location}
@@ -88,19 +153,37 @@ export default class PerformerView extends PureComponent {
       />
     );
 
+    // 生成页码器，此页码器配置项与Antd的一致
+    const { location: { query: { pageNum = 1, pageSize = 10 } } } = this.props;
+    const { resultData = [], page = {} } = list;
+    const paginationOptions = {
+      current: parseInt(pageNum, 10),
+      defaultCurrent: 1,
+      size: 'small', // 迷你版
+      total: page.totalCount || 0,
+      pageSize: parseInt(pageSize, 10),
+      defaultPageSize: 10,
+      onChange: this.handlePageNumberChange,
+      showTotal: appListTool.showTotal,
+      showSizeChanger: true,
+      onShowSizeChange: this.handlePageSizeChange,
+      pageSizeOptions: appListTool.constructPageSizeOptions(page.totalCount || 0),
+    };
+
     const leftPanel = (
       <PerformerViewList
-        list={performerViewList}
-        replace={replace}
-        location={location}
-        pageName="performerView"
-        type="kehu1"
-        pageData={performerView}
+        list={resultData}
+        renderRow={this.renderListRow}
+        pagination={paginationOptions}
       />
     );
 
     const rightPanel = (
-      <PerformerViewDetail />
+      <PerformerViewDetail
+        dict={dict}
+        isReadOnly={false}
+        addServeRecord={addServeRecord}
+      />
     );
     return (
       <div>
