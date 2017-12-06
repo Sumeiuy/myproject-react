2e14cd32c48998d4676e92c5e80dfe04f9323c11
K0240008 <junjun.wang@cienet.com.cn>
2017-12-06 14:44:36 +0800

    #FSP-7535#bug fix

--- src/routes/taskList/Home.js
+++ src/routes/taskList/Home.js
@@ -596,6 +596,7 @@ export default class PerformerView extends PureComponent {
           leftPanel={leftPanel}
           rightPanel={rightPanel}
           leftListClassName="premissionList"
+          leftWidth={420}
         />
       </div>
     );
