216921c19ec7a170c387f9c6108aa6e26fe40535
K0180004 <hongguangqing34615@billjc.com>
2017-12-14 15:19:23 +0800

    #FSP-8380#私密客户修改弹框改为路由页面的样式调整

--- src/helper/emp.js
+++ src/helper/emp.js
@@ -2,7 +2,7 @@
  * @Author: sunweibin
  * @Date: 2017-11-22 10:06:59
  * @Last Modified by: hongguangqing
- * @Last Modified time: 2017-12-14 10:55:03
+ * @Last Modified time: 2017-12-14 15:17:56
  * @description 此处存放与系统登录人相关的公用方法
  */
 import qs from 'query-string';
@@ -16,7 +16,7 @@ const emp = {
    */
   getId() {
     // 临时 ID
-    const tempId = '001206'; // '001423''002727','002332' '001206' '001410';
+    const tempId = '002332'; // '001423''002727','002332' '001206' '001410';
     const nativeQuery = qs.parse(window.location.search);
     const empId = window.curUserCode || nativeQuery.empId || tempId;
     return empId;
