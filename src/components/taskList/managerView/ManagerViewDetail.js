5e562347260413a634ba9c83a362545fb4eb4346
K0240010 <xuxiaoqin@test.com.cn>
2017-12-21 15:12:19 +0800

    #FSP-000#对照测试用例修改

--- src/components/taskList/managerView/ManagerViewDetail.js
+++ src/components/taskList/managerView/ManagerViewDetail.js
@@ -2,7 +2,7 @@
  * @Author: xuxiaoqin
  * @Date: 2017-12-04 14:08:41
  * @Last Modified by: xuxiaoqin
- * @Last Modified time: 2017-12-21 14:33:19
+ * @Last Modified time: 2017-12-21 14:38:14
  * 管理者视图详情
  */
 
