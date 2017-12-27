cd2bcc4834e54dee9b4d10fde47e667e7ac2c6a4
K0240010 <xuxiaoqin@test.com.cn>
2017-12-27 14:00:48 +0800

    #FSP-8362#首页指标样式调整

--- src/components/customerPool/createTask/CreateTaskSuccess.js
+++ src/components/customerPool/createTask/CreateTaskSuccess.js
@@ -10,7 +10,6 @@ import ReactDOM from 'react-dom';
 import styles from './createTaskSuccess.less';
 import Clickable from '../../../components/common/Clickable';
 import imgSrc from '../../../../static/images/createTask_success.png';
-import { fspGlobal } from '../../../utils';
 import { fspContainer } from '../../../config';
 import Button from '../../common/Button';
 
@@ -21,13 +20,11 @@ export default class CreateTaskSuccess extends PureComponent {
     onCloseTab: PropTypes.func.isRequired,
     location: PropTypes.object.isRequired,
     clearSubmitTaskFlowResult: PropTypes.func,
-    onRemoveTab: PropTypes.func,
   }
 
   static defaultProps = {
     successType: false,
-    clearSubmitTaskFlowResult: () => {},
-    onRemoveTab: null,
+    clearSubmitTaskFlowResult: () => { },
   }
 
   constructor(props) {
@@ -69,14 +66,9 @@ export default class CreateTaskSuccess extends PureComponent {
   @autobind
   goToHome() {
     this.clearTimeInterval();
-    const { onCloseTab, onRemoveTab, push, location: { state, query } } = this.props;
+    const { onCloseTab, push, location: { state, query } } = this.props;
     if (document.querySelector(fspContainer.container)) {
-      if(typeof onRemoveTab === 'function') {
-        onRemoveTab();
-        fspGlobal.switchFspTab('tab-home');
-      } else {
-        onCloseTab();
-      }
+      onCloseTab();
     } else {
       push({
         pathname: '/customerPool',
