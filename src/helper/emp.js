8a3ff13ab0e8f34bdfb7f8ef5fa098ca92feeda7
K0240010 <xuxiaoqin@test.com.cn>
2018-01-24 17:24:13 +0800

    #FSP-000#联调自建任务权限控制

--- src/helper/emp.js
+++ src/helper/emp.js
@@ -2,7 +2,7 @@
  * @Author: sunweibin
  * @Date: 2017-11-22 10:06:59
  * @Last Modified by: xuxiaoqin
- * @Last Modified time: 2018-01-24 13:22:52
+ * @Last Modified time: 2018-01-24 16:03:18
  * @description 此处存放与系统登录人相关的公用方法
  */
 import qs from 'query-string';
@@ -86,6 +86,7 @@ const emp = {
    * @returns {String|null} 职位信息
    */
   getPstnId() {
+    // 岗位Id，供本地使用，
     let pstnId = '1-3NQ97YG';
     if (!_.isEmpty(window.forReactPosition)) {
       pstnId = window.forReactPosition.pstnId;
