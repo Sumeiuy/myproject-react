a2b3c996d2d6395edb8ed26e73ee7a7231f88424
K0240008 <junjun.wang@cienet.com.cn>
2018-01-29 09:54:39 +0800

    #FSP-7535#自建任务瞄准镜

--- config/index.js
+++ config/index.js
@@ -45,27 +45,26 @@ module.exports = {
     proxyTable: generateProxy([
       prefix + '/morningBoradcast',
       {
-        // target: 'http://168.61.8.81:5089', // SIT
+        target: 'http://168.61.8.81:5089', // SITf
         //  target: 'http://168.61.8.81:5090', // DOClever
-        target: 'http://160.9.230.146:8082/', // 张宝成 接口访问地址                
+        // target: 'http://160.9.230.146:8082/', // 张宝成 接口访问地址                
       },
       prefix,
       {
-        // target: 'http://168.61.8.81:5086', // SIT
+        target: 'http://168.61.8.81:5086', // SIT
         // target: 'http://168.61.8.81:5090', // DOClever
-        target: 'http://160.9.230.146:8082', // 张宝成本地地址
+        // target: 'http://160.9.230.146:8082', // 张宝成本地地址
       },
       '/fspa/log',
       {
-        target: 'http://160.9.230.146:8082', // 张宝成本地地址
-        // target: 'http://168.61.8.81:5086', // SIT
+        // target: 'http://160.9.230.146:8082', // 张宝成本地地址
+        target: 'http://168.61.8.81:5086', // SIT
       },
       '/fsp',
       {
-        target: 'http://160.9.230.146:8082', // 张宝成本地地址
-        // target: 'http://168.61.8.81:5086', // SIT
-        // target: 'http://168.61.8.82:5086', // UAT
-        // target: 'http://160.9.230.9:8082/', // 张宝成 接口访问地址        
+        // target: 'http://160.9.230.146:8082', // 张宝成本地地址
+        target: 'http://168.61.8.81:5086', // SIT
+        // target: 'http://168.61.8.82:5086', // UAT     
       }
     ]),
     // CSS Sourcemaps off by default because relative paths are "buggy"
