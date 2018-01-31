a6351488d04e3a619077d1c5f0c35ff2a6475a94
K0240008 <junjun.wang@cienet.com.cn>
2018-01-31 16:11:41 +0800

    #FSP-7535#去掉多余的console.log

--- src/components/common/serviceRecordContent/index.js
+++ src/components/common/serviceRecordContent/index.js
@@ -594,7 +594,6 @@ export default class ServiceRecordContent extends PureComponent {
       [styles.serveType]: true,
       [styles.hidden]: isEntranceFromPerformerView,
     });
-    console.log('look here: ', feedbackTypeArr, feedbackType, feedbackTypeChildArr, feedbackTypeChild);
     // feedbackTypeChildArr为空或者客户反馈一级和二级的选项文字相同时不显示二级反馈选项
     let isShowSubCustomerFeedback = false;
     if (!_.isEmpty(feedbackTypeChildArr)) {
