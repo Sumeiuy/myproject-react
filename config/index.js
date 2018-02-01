ea77e179d1dae5b9cc393d248c9fc737c5b18d03
k0180004 <k0180004@test.htsc.com.cn>
2018-02-01 11:48:49 +0800

    #FSP-9298#分公司客户划转新建页面

--- config/index.js
+++ config/index.js
@@ -51,13 +51,8 @@ module.exports = {
       },
       prefix,
       {
-<<<<<<< HEAD
         // target: 'http://168.61.8.81:5087', // SIT
         target: 'http://168.61.8.81:5090', // DOClever
-=======
-        target: 'http://168.61.8.81:5086', // SIT
-        // target: 'http://168.61.8.81:5090', // DOClever
->>>>>>> 9eed73ee6bc364f9e37438addd2997f896b1bf71
         // target: 'http://160.9.230.146:8082/', // 张宝成 接口访问地址
       },
       '/fspa/log',
