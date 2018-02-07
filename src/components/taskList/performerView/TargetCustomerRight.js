3efa1c4e97bb3ce954ddb1d8337235361565ab69
K0240010 <xuxiaoqin@test.com.cn>
2018-02-07 15:31:09 +0800

    #FSP-000#修改执行者视图与管理者视图bug

--- src/components/taskList/performerView/TargetCustomerRight.js
+++ src/components/taskList/performerView/TargetCustomerRight.js
@@ -283,7 +283,7 @@ export default class TargetCustomerRight extends PureComponent {
               </Col>
             </Row>
             <Row className={styles.mt3}>
-              <Col span={12}>
+              <Col span={8}>
                 <h5
                   className={styles.phoneLeft}
                 >
@@ -299,7 +299,7 @@ export default class TargetCustomerRight extends PureComponent {
               </Col>
               {
                 itemData.contactPhone ?
-                  <Col span={12}>
+                  <Col span={16}>
                     <h5
                       className={styles.phoneRight}
                     >
