7292bfb37eac73b6ca9994a79f049c3d098d4eb8
K0100010 <k0100010@test.htsc.com.cn>
2018-02-08 17:39:35 +0800

    #FSP-9319#根据需求调整目标客户池首页资讯UI

--- config/index.js
+++ config/index.js
@@ -52,8 +52,9 @@ module.exports = {
       },
       prefix,
       {
+        target: 'http://168.61.8.82:5086', // uat
         // target: 'http://168.61.8.81:5087', // SIT
-        target: 'http://168.61.8.81:5090', // DOClever
+        // target: 'http://168.61.8.81:5090', // DOClever
         // target: 'http://160.9.230.146:8082/', // 张宝成 接口访问地址
       },
       '/fspa/log',
