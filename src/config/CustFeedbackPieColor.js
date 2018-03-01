705f6f77f92a360600d389b2527dd1eb5e92f182
K0180039 <K0180039@test.htsc.com.cn>
2018-03-01 19:27:48 +0800

    #FSP-000#饼图颜色修改

---
+++ src/config/CustFeedbackPieColor.js
@@ -0,0 +1,94 @@
+export default {};
+
+// 客户反馈饼图色值集合
+export const custFeedbackColorCollection = [
+  {
+    key: 0,
+    value: [
+      '#3943FF',
+      '#5089F4',
+      '#39B6FF',
+      '#39D8FF',
+      '#8DF0F0',],
+  },
+  {
+    key: 1,
+    value: [
+      '#15A82A',
+      '#43CD57',
+      '#99DD6C',
+      '#C3DA4A',
+      '#DAD94A',
+    ],
+  },
+  {
+    key: 2,
+    value: [
+      '#5E1EA8',
+      '#883AE2',
+      '#AE74F1',
+      '#C8A6F0',
+      '#E0D5ED',
+    ],
+  },
+  {
+    key: 3,
+    value: [
+      '#CB1717',
+      '#FF4E4E',
+      '#F78C8C',
+      '#FEB6B6',
+      '#FED7D7',
+    ],
+  },
+  {
+    key: 4,
+    value: [
+      '#D17701',
+      '#ED992C',
+      '#FBBC6B',
+      '#FFD9A7',
+      '#F5E2C9',
+    ],
+  },
+  {
+    key: 5,
+    value: [
+      '#0E6D01',
+      '#2FAE1E',
+      '#67D757',
+      '#96ED8B',
+      '#D1EFCD',
+    ],
+  },
+  {
+    key: 6,
+    value: [
+      '#B69700',
+      '#D9B921',
+      '#F1D652',
+      '#ECE993',
+      '#E8E7CD',
+    ],
+  },
+  {
+    key: 7,
+    value: [
+      '#527C2E',
+      '#61AC22',
+      '#83D042',
+      '#B6E98A',
+      '#D5E5C8',
+    ],
+  },
+  {
+    key: 8,
+    value: [
+      '#FF5C19',
+      '#EF723F',
+      '#F99F7A',
+      '#FBC2AB',
+      '#F5DFD6',
+    ],
+  },
+];
