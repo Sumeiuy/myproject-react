ce963c306139cf03a5284f900a323a328b79de3a
K0170159 <ouchangzhi@git.com.cn>
2018-03-19 11:50:45 +0800

    #FSP-10204#删除console

--- src/components/customerPool/home/ManageIndicators.js
+++ src/components/customerPool/home/ManageIndicators.js
@@ -70,8 +70,6 @@ export default class PerformanceIndicators extends PureComponent {
         empInfo,
         permissionType,
       } = this.props;
-      // console.log('arg>>', arg);
-      // console.log('clientNameData: ', clientNameData);
 
       const param = {
         source: 'numOfCustOpened',
