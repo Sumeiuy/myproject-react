cae9dc99c68becc8fac98df8c6a7e52e222ff3ee
K0240008 <junjun.wang@cienet.com.cn>
2018-03-22 11:44:15 +0800

    #FSP-7535#任务 管理修改

---
+++ src/routes/taskList/config.js
@@ -0,0 +1,40 @@
+import moment from 'moment';
+
+const dateFormat = 'YYYY-MM-DD';
+
+const EXECUTOR = 'executor'; // 执行者视图
+const INITIATOR = 'initiator'; // 创造者视图
+const CONTROLLER = 'controller'; // 管理者视图
+
+const chooseMissionView = [
+  {
+    show: true,
+    label: '我创建的任务',
+    value: 'initiator',
+  },
+  {
+    show: true,
+    label: '我执行的任务',
+    value: 'executor',
+  },
+  {
+    show: true,
+    label: '我部门的任务',
+    value: 'controller',
+  },
+];
+
+const currentDate = moment(new Date());
+const beforeCurrentDate60Days = moment(currentDate).subtract(60, 'days');
+const afterCurrentDate60Days = moment(currentDate).add(60, 'days');
+
+export default {
+  EXECUTOR,
+  INITIATOR,
+  CONTROLLER,
+  chooseMissionView,
+  currentDate,
+  beforeCurrentDate60Days,
+  afterCurrentDate60Days,
+  dateFormat,
+};
