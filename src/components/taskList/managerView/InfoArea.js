b2ea195b3ee72d74e5a9a18938cb12f78c49597d
K0170179 <k0170179@test.htsc.com.cn>
2018-03-28 19:40:25 +0800

    #FSP-10207# codereview fixed

--- src/components/taskList/managerView/InfoArea.js
+++ src/components/taskList/managerView/InfoArea.js
@@ -23,6 +23,7 @@ function InfoArea(props) {
           _.map(
             data,
             (item) => {
+              // policy or tip 是 服务策略 和 任务提示项设置的ID
               const isPoliceOrTip = item.id === 'policy' || item.id === 'tip';
               return (
                 <div
@@ -37,7 +38,6 @@ function InfoArea(props) {
                   >{item.key}</div>
                   <div className={styles.infoValue}>
                     {
-                      // policy or tip 是 服务策略 和 任务提示项设置的ID
                       isPoliceOrTip ?
                         <div
                           className={styles.row}
