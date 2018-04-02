3ce34f72a450e74fcaccce226537a40dc1323262
K0100010 <k0100010@test.htsc.com.cn>
2018-04-02 17:58:55 +0800

    #FSP-10458#移除无用得代码

--- src/components/pageCommon/SelfDatePicker.js
+++ src/components/pageCommon/SelfDatePicker.js
@@ -236,7 +236,6 @@ export default class SelfDatePicker extends PureComponent {
     },
   })
   rangePickerChange(dates, dateStrings) {
-    console.log('########选的时间段事件#############', dates, dateStrings);
     const { compare } = this.state;
     const beginMoment = dates[0];
     const endMoment = dates[1];
