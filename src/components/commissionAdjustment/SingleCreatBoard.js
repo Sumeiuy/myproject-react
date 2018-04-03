13225602bd5358ca9d60c23f0a312907f2bb2940
k0180004 <k0180004@test.htsc.com.cn>
2018-04-03 15:16:36 +0800

    #FSP-10544#修改不知道是谁提交上去的eslint报错

--- src/components/commissionAdjustment/SingleCreatBoard.js
+++ src/components/commissionAdjustment/SingleCreatBoard.js
@@ -422,7 +422,7 @@ export default class SingleCreateBoard extends PureComponent {
       ref: this.uploadRef,
     };
 
-    const newCurrentCom = _.isEmpty(customer) ? '--' :`${newCurrentCommission}‰`;
+    const newCurrentCom = _.isEmpty(customer) ? '--' : `${newCurrentCommission}‰`;
 
     return (
       <div className={styles.contentBox}>
