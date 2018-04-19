0c1cdfd54df2fcea10b67e26bb2a179270ba5cce
K0050045 <xuwenkang@chinasoft.com>
2018-04-19 16:06:46 +0800

    #FSP-10882# 精选组合开发

--- src/components/choicenessCombination/combinationRank/CombinationListItem.js
+++ src/components/choicenessCombination/combinationRank/CombinationListItem.js
@@ -3,7 +3,7 @@
  * @Description: 精选组合-组合排名-列表项
  * @Date: 2018-04-18 14:26:13
  * @Last Modified by: XuWenKang
- * @Last Modified time: 2018-04-18 16:54:13
+ * @Last Modified time: 2018-04-19 14:09:56
 */
 
 import React, { PureComponent } from 'react';
@@ -26,42 +26,49 @@ export default class CombinationListItem extends PureComponent {
 
   render() {
     return (
-      <div className={styles.itemBox}>
-        <div className={styles.headBox}>
-          <span className={styles.combinationName} title={'组合名称'}>组合名称组合名称</span>
-          <span className={styles.earnings}>
-            <i>近7天收益率</i>
-            <em className={styles.up}>+5.12%</em>
-          </span>
-          <span className={styles.tips}>
-            <i>高风险</i>
-            <em>推荐</em>
-          </span>
-          <span className={styles.link}>
-            <a>历史报告</a>
-            |
-            <a>订购客户</a>
-          </span>
+      <div className={`${styles.itemBox} clearfix`}>
+        <div className={styles.left}>
+          <div className={styles.headBox}>
+            <span className={styles.combinationName} title={'组合名称'}>
+              <a>组合名称组合名称</a>
+            </span>
+            <span className={styles.earnings}>
+              <i>近7天收益率</i>
+              <em className={styles.up}>+5.12%</em>
+            </span>
+            <span className={styles.tips}>
+              <i>高风险</i>
+              <em>推荐</em>
+            </span>
+            <span className={styles.link}>
+              <a>历史报告</a>
+              |
+              <a>订购客户</a>
+            </span>
+          </div>
+          <div className={styles.titleBox}>
+            <span className={styles.securityName}>证券名称</span>
+            <span className={styles.securityCode}>证券代码</span>
+            <span className={styles.direction}>调仓方向</span>
+            <span className={styles.time}>时间</span>
+            <span className={styles.cost}>成本价</span>
+            <span className={styles.reason}>理由</span>
+          </div>
+          <div className={styles.bodyBox}>
+            <span className={styles.securityName}>
+              <a>证券名称</a>
+            </span>
+            <span className={styles.securityCode}>
+              <a>证券名称</a>
+            </span>
+            <span className={styles.direction}>买</span>
+            <span className={styles.time}>2018/02/12 12:00</span>
+            <span className={styles.cost}>3.20</span>
+            <span className={styles.reason}>理由理由理由理由理由</span>
+          </div>
         </div>
-        <div className={styles.titleBox}>
-          <span className={styles.securityName}>证券名称</span>
-          <span className={styles.securityCode}>证券代码</span>
-          <span className={styles.direction}>调仓方向</span>
-          <span className={styles.time}>时间</span>
-          <span className={styles.cost}>成本价</span>
-          <span className={styles.reason}>理由</span>
-        </div>
-        <div className={styles.bodyBox}>
-          <span className={styles.securityName}>
-            <a>证券名称</a>
-          </span>
-          <span className={styles.securityCode}>
-            <a>证券名称</a>
-          </span>
-          <span className={styles.direction}>买</span>
-          <span className={styles.time}>2018/02/12 12:00</span>
-          <span className={styles.cost}>3.20</span>
-          <span className={styles.reason}>理由理由理由理由理由</span>
+        <div className={styles.right}>
+          图表
         </div>
       </div>
     );
