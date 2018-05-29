6f4cd295b454e53b4d10470fba20d80b8d31b0fe
K0100010 <k0100010@test.htsc.com.cn>
2018-05-29 14:39:50 +0800

    #FSP-12262#随后端接口修改逻辑：终止时，也需要传下一步审批人

--- config/index.js
+++ config/index.js
@@ -57,7 +57,7 @@ module.exports = {
         // target: 'http://168.61.8.82:5085', // SIT
         // target: 'http://168.61.8.81:5090', // DOClever
         // target: 'http://160.9.230.9:8082', // Java开发接口访问地址
-        // target: 'http://160.10.6.250:8082',
+        // target: 'http://160.9.230.159:8082', // 王必强
       },
       '/fspa/log',
       {
