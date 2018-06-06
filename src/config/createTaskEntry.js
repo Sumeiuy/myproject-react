7978df7dfec31227e1ac620e794181437e8c3ea4
K0240010 <k0240010@test.htsc.com.cn>
2018-06-06 20:03:14 +0800

    #FSP-00000#修改代码不合理地方

--- src/config/createTaskEntry.js
+++ src/config/createTaskEntry.js
@@ -1,52 +1,51 @@
 export default {};
 
 // returnTaskFromToDoList是待办，审批驳回之后，编辑自建任务信息界面
-// returnTaskFromTaskList是创建者视图，审批驳回之后，编辑自建任务信息界面
-// custGroupList是客户分组
-// pieEntry是管理者视图的饼图
-// progressEntry是管理者视图的进度条
-// business从首页潜在目标客户下钻进来
-// search从首页搜索进来
-// productPotentialTargetCust从外部入口，产品潜在客户下钻，带过来瞄准镜标签
-// securitiesProducts从精选组合的证券产品产品中心进来
-// orderCombination从精选组合的证券产品订购组合进来
-// external从外部入口进来，非理财平台，不是瞄准镜标签
-// association从首页搜索下拉列表进来
-// tag从首页热点标签进来
-// custIndicator从绩效目标客户池进来
-// numOfCustOpened从绩效目标客户池进来
-// sightingTelescope从首页搜索，搜索一个瞄准镜标签或者从热点标签，这个标签是瞄准镜的标签
-
 export const RETURN_TASK_FROM_TODOLIST = 'returnTaskFromToDoList';
 
+// returnTaskFromTaskList是创建者视图，审批驳回之后，编辑自建任务信息界面
 export const RETURN_TASK_FROM_TASKLIST = 'returnTaskFromTaskList';
 
+// custGroupList是客户分组
 export const CUST_GROUP_LIST = 'custGroupList';
 
+// pieEntry是管理者视图的饼图
 export const PIE_ENTRY = 'pieEntry';
 
+// progressEntry是管理者视图的进度条
 export const PROGRESS_ENTRY = 'progressEntry';
 
+// business从首页潜在目标客户下钻进来
 export const BUSINESS_ENTRY = 'business';
 
+// search从首页搜索进来
 export const SEARCH_ENTRY = 'search';
 
+// productPotentialTargetCust从外部入口，产品潜在客户下钻，带过来瞄准镜标签
 export const PRODUCT_POTENTIAL_TARGET_CUST_ENTRY = 'productPotentialTargetCust';
 
+// securitiesProducts从精选组合的证券产品产品中心进来
 export const SECURITIES_PRODUCTS_ENTRY = 'securitiesProducts';
 
+// orderCombination从精选组合的证券产品订购组合进来
 export const ORDER_COMBINATION_ENTRY = 'orderCombination';
 
+// external从外部入口进来，非理财平台，不是瞄准镜标签
 export const EXTERNAL_ENTRY = 'external';
 
+// association从首页搜索下拉列表进来
 export const ASSOCIATION_ENTRY = 'association';
 
+// tag从首页热点标签进来
 export const TAG_ENTRY = 'tag';
 
+// custIndicator从绩效目标客户池进来
 export const CUSTINDICATOR_ENTRY = 'custIndicator';
 
+// numOfCustOpened从绩效目标客户池进来
 export const NUMOFCUSTOPENED_ENTRY = 'numOfCustOpened';
 
+// sightingTelescope从首页搜索，搜索一个瞄准镜标签或者从热点标签，这个标签是瞄准镜的标签
 export const SIGHTINGTELESCOPE_ENTRY = 'sightingTelescope';
 
 // 所有可以发起任务的入口集合
