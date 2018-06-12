368b6876798a6249ef1b6a31824a6508e0e244d4
K0240010 <k0240010@test.htsc.com.cn>
2018-06-12 17:39:15 +0800

    #FSP-00000#移除管理者视图服务经理维度发起任务优先，其它开发分支已提交

--- src/components/taskList/managerView/ManagerViewDetail.js
+++ src/components/taskList/managerView/ManagerViewDetail.js
@@ -2,7 +2,7 @@
  * @Author: xuxiaoqin
  * @Date: 2017-12-04 14:08:41
  * @Last Modified by: xuxiaoqin
- * @Last Modified time: 2018-06-12 17:24:18
+ * @Last Modified time: 2018-06-12 17:37:53
  * 管理者视图详情
  */
 
@@ -380,12 +380,7 @@ export default class ManagerViewDetail extends PureComponent {
     let currentEntryName = '';
     let currentEntryId = '';
     let currentRoute = '';
-    // 服务经理维度发起任务优先
-    if (!_.isEmpty(enterType)) {
-      currentEntryName = TASK_CUST_SCOPE_ENTRY;
-      currentEntryId = 'RCT_FSP_CREATE_TASK_FROM_MANAGERVIEW_CUST_SCOPE';
-      currentRoute = '/customerPool/createTaskFromCustScope';
-    } else if (isEntryFromPie) {
+    if (isEntryFromPie) {
       currentEntryName = PIE_ENTRY;
       currentEntryId = 'RCT_FSP_CREATE_TASK_FROM_MANAGERVIEW_CUSTFEEDBACK_PIE';
       currentRoute = '/customerPool/createTaskFromPie';
@@ -393,6 +388,10 @@ export default class ManagerViewDetail extends PureComponent {
       currentEntryName = PROGRESS_ENTRY;
       currentEntryId = 'RCT_FSP_CREATE_TASK_FROM_MANAGERVIEW_CUSTFEEDBACK_PROGRESS';
       currentRoute = '/customerPool/createTaskFromProgress';
+    } else if (!_.isEmpty(enterType)) {
+      currentEntryName = TASK_CUST_SCOPE_ENTRY;
+      currentEntryId = 'RCT_FSP_CREATE_TASK_FROM_MANAGERVIEW_CUST_SCOPE';
+      currentRoute = '/customerPool/createTaskFromCustScope';
     }
 
     // 发起新的任务之前，先清除数据
