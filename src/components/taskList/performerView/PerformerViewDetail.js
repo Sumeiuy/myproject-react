9dbd4d3fe607a7f3723f9bd6d55efd72dfc6e166
K0240008 <wangjunjun40829@billjc.com>
2018-06-14 09:53:52 +0800

    '#FSP-7535#执行者视图右侧服务实施列表不更新的问题'

--- src/components/taskList/performerView/PerformerViewDetail.js
+++ src/components/taskList/performerView/PerformerViewDetail.js
@@ -3,7 +3,7 @@
  * @Author: WangJunjun
  * @Date: 2018-05-22 12:25:35
  * @Last Modified by: WangJunjun
- * @Last Modified time: 2018-06-14 09:00:56
+ * @Last Modified time: 2018-06-14 09:46:34
  */
 
 
@@ -106,16 +106,29 @@ export default class PerformerViewDetail extends PureComponent {
   }
 
   componentDidMount() {
+    this.getServiceImplementationList(this.props);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.currentId !== this.props.currentId) {
+      this.getServiceImplementationList(this.props);
+    }
+  }
+
+  // 获取服务实施列表
+  @autobind
+  getServiceImplementationList(props) {
     const {
       queryTargetCust,
       currentId,
       isFold,
-    } = this.props;
+    } = props;
     const isFoldFspLeftMenu = fsp.isFSPLeftMenuFold();
     const newPageSize = getPageSize(isFoldFspLeftMenu, isFold);
     // 执行者视图服务实施客户列表中 状态筛选默认值 state='10' 未开始
     queryTargetCust({ missionId: currentId, state: '10', pageNum: 1, pageSize: newPageSize });
   }
+  
 
   // 生成基本信息中的内容
   @autobind
