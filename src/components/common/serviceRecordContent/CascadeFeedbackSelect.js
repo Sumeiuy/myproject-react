a8ebaf0eddca67899537c399c2bdfc9e50d8dbc2
K0240010 <k0240010@test.htsc.com.cn>
2018-06-14 16:46:44 +0800

    #FSP-00000#服务记录提交之后，不显示二级客户反馈

--- src/components/common/serviceRecordContent/CascadeFeedbackSelect.js
+++ src/components/common/serviceRecordContent/CascadeFeedbackSelect.js
@@ -2,7 +2,7 @@
  * @Author: sunweibin
  * @Date: 2018-04-14 20:52:53
  * @Last Modified by: xuxiaoqin
- * @Last Modified time: 2018-06-14 14:13:59
+ * @Last Modified time: 2018-06-14 16:46:04
  * @description 非涨乐财富通服务方式下的客户反馈级联Select
  */
 import React, { PureComponent } from 'react';
@@ -33,6 +33,7 @@ export default class CascadeFeedbackSelect extends PureComponent {
 
   componentWillReceiveProps(nextProps) {
     const { value } = nextProps;
+    // 外部变化，不显示二级反馈
     this.setState({ ...value });
   }
 
@@ -131,9 +132,11 @@ export default class CascadeFeedbackSelect extends PureComponent {
           {/**
            * isShowSecond
            * 代表是否展示二级反馈
+           * first === defaultFeedbackOption
+           * 用来更新组件时，不显示客户反馈
            */}
           {
-            !isShowSecond ? null :
+            !isShowSecond || first === defaultFeedbackOption ? null :
               (<Select
                 value={second}
                 style={{ width: 142 }}
