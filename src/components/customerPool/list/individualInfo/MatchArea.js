707cf9653d9de5d7bb4bc6ae66065e2ca23c517a
K0170179 <k0170179@test.htsc.com.cn>
2018-06-28 11:44:42 +0800

    #FSP-13064# 个性化信息

--- src/components/customerPool/list/individualInfo/MatchArea.js
+++ src/components/customerPool/list/individualInfo/MatchArea.js
@@ -371,7 +371,7 @@ export default class MatchArea extends PureComponent {
       listItem,
     } = this.props;
     if (matchLabels && !matchLabels.length) {
-      return [];
+      return null;
     }
     const { searchText = '' } = this.getFilters();
     if (!_.isEmpty(listItem.relatedLabels)) {
@@ -676,7 +676,7 @@ export default class MatchArea extends PureComponent {
       const normalListNode = this.renderRelatedLabels(normalLabelList);
       return [normalListNode, ...amiListNode];
     }
-    return [];
+    return null;
   }
 
   @autobind
