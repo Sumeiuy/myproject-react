f69b613505c179762d1cf9cffe22a4f488a36043
K0170179 <k0170179@test.htsc.com.cn>
2018-07-11 16:51:57 +0800

    #FSP-13064# 首次进入平台参数设置子页面的控制

--- src/routes/platformParameterSetting/Home.js
+++ src/routes/platformParameterSetting/Home.js
@@ -8,7 +8,9 @@ import React, { PureComponent } from 'react';
 import { Route, Switch, Redirect } from 'dva/router';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
+import { autobind } from 'core-decorators';
 import menu from './menu';
+import withRouter from '../../decorators/withRouter';
 import Main from '../../components/platformParameterSetting/Main';
 import CustomerFeedback from '../../routes/customerFeedback/Home';
 import TaskFeedback from '../taskFeedback/Home';
@@ -18,23 +20,58 @@ import { LabelManager,
   RecommendedLabel,
 } from '../../components/platformParameterSetting';
 
+@withRouter
 export default class PlatformParameterSetting extends PureComponent {
   static propTypes = {
     match: PropTypes.object.isRequired,
+    location: PropTypes.object.isRequired,
   };
   static contextTypes= {
     empInfo: PropTypes.object.isRequired,
+    replace: PropTypes.func.isRequired,
   }
-  render() {
-    const { match: { path } } = this.props;
+
+  componentDidMount() {
+    // 根据路由权限过滤后平台参数设置菜单下首页面的跳转
+    this.toPlatformHome();
+  }
+
+  @autobind
+  hasPermissionMenu() {
     const { empInfo: { empRespList = [] } } = this.context;
-    const premissionList = empRespList.map(item => item.respId);
-    const finalMenu = _.filter(menu, (item) => {
+    const permissionList = empRespList.map(item => item.respId);
+    return _.filter(menu, (item) => {
       if (!item.permission) {
         return true;
       }
-      return _.includes(premissionList, item.permission);
+      return _.includes(permissionList, item.permission);
     });
+  }
+
+  toPlatformHome() {
+    const { match: { path }, location: { pathname, query } } = this.props;
+    const { replace } = this.context;
+    let homePath = path;
+    if (path !== pathname) {
+      return;
+    }
+    const finalMenu = this.hasPermissionMenu();
+    const getHomePath = (matchMenu) => {
+      if (_.isArray(matchMenu) && matchMenu.length) {
+        const firstMenuItem = matchMenu[0];
+        const { path: pathItem, children } = firstMenuItem;
+        homePath = `${homePath}${pathItem}`;
+        getHomePath(children);
+      }
+    };
+    getHomePath(finalMenu);
+    replace({ pathname: homePath, query });
+  }
+
+  render() {
+    const { match: { path } } = this.props;
+    const finalMenu = this.hasPermissionMenu();
+
     return (
       <Main menu={finalMenu} matchPath={path}>
         <Switch>
