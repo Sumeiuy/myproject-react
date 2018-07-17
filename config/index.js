c0130b6eefd034bab067bc8d8a0d2969f98e2d1c
K0100008 <weibin.sun@archermind.com>
2018-07-17 08:56:39 +0800

    #FSP-13708#涨乐财富通固定话术模板创建服务记录

--- config/index.js
+++ config/index.js
@@ -51,24 +51,6 @@ module.exports = {
       {
         target: 'http://168.61.8.82:5086', // uat
       },
-      prefix + '/groovynoauth/fsp/biz/closeaccountonline',
-      {
-        // target: 'http://168.61.8.82:5086', // uat
-        // target: 'http://168.61.8.81:5087', // SIT
-        target: 'http://168.61.8.81:5090', // DOClever
-      },
-      prefix + '/groovynoauth/fsp/cust/closeaccountonline',
-      {
-        // target: 'http://168.61.8.82:5086', // uat
-        // target: 'http://168.61.8.81:5087', // SIT
-        target: 'http://168.61.8.81:5090', // DOClever
-      },
-      prefix + '/groovynoauth/fsp/biz/queryApplicationList2',
-      {
-        // target: 'http://168.61.8.82:5086', // uat
-        // target: 'http://168.61.8.81:5087', // SIT
-        target: 'http://168.61.8.81:5090', // DOClever
-      },
       prefix,
       {
         target: 'http://168.61.8.82:5086', // uat
