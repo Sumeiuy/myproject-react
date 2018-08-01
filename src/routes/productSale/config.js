caaecb54e6cf3de220cc2eef27f288e00397289a
K0170222 <zhangjun@git.com.cn>
2018-08-01 17:47:43 +0800

    #FSP-13923#运维管理平台参数增加产品销售开发

---
+++ src/routes/productSale/config.js
@@ -0,0 +1,17 @@
+const KEYFIRSTPUBLICOFFERING = '/sysOperate/platformParameterSetting/productSale/keyFirstPublicOffering';
+const FILIALEANNUALTARGET = '/sysOperate/platformParameterSetting/productSale/filialeAnnualTarget';
+const IFRAMEPATH = '/acrmbi/login?menuId=';
+
+export default {
+  iframe: [
+    {
+      path: KEYFIRSTPUBLICOFFERING,
+      name: '重点首发公募',
+      iframePath: `${IFRAMEPATH}01064`,
+    }, {
+      path: FILIALEANNUALTARGET,
+      name: '分公司年度目标',
+      iframePath: `${IFRAMEPATH}01065`,
+    },
+  ],
+};
