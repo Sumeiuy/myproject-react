d8beda2ea100b7a9593ee8e4bdd65d21bc3effdc
K0100008 <k0100008@test.htsc.com.cn>
2018-08-23 15:15:27 +0800

    #FSP-14176#修改MOT回访类型人物的event_id

--- src/config/taskList/performView.js
+++ src/config/taskList/performView.js
@@ -1,13 +1,13 @@
 /**
  * @Author: sunweibin
  * @Date: 2018-08-09 10:45:27
- * @Last Modified by: mikey.zhaopeng
- * @Last Modified time: 2018-08-15 16:11:59
+ * @Last Modified by: sunweibin
+ * @Last Modified time: 2018-08-23 15:13:09
  * @description 执行者视图下的相关配置项
  */
 
 // MOT 回访类任务类型的 eventId
-export const MOT_RETURN_VISIT_TASK_EVENT_ID = '100445';
+export const MOT_RETURN_VISIT_TASK_EVENT_ID = 'mot_temp_event_id';
 // MOT 回访类型任务回访结果
 export const MOT_RETURN_VISIT_WORK_RESULT_SUCCESS = '成功';
 export const MOT_RETURN_VISIT_WORK_RESULT_LOST = '失败';
