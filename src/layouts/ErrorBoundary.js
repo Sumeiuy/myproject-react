d94d9e3c08b41232662f2d1d97449bf67dc1bd2d
K0050051 <k0050051@test.htsc.com.cn>
2018-08-28 18:02:48 +0800

    #FSP-11234#修正ErrorBoundary回置到无错误状态的逻辑

--- src/layouts/ErrorBoundary.js
+++ src/layouts/ErrorBoundary.js
@@ -17,16 +17,21 @@ export default class ErrorBoundary extends Component {
     location: PropTypes.object.isRequired,
   };
 
+  static getDerivedStateFromProps(nextProps, prevState) {
+    if (prevState.location.pathname !== nextProps.location.pathname) {
+      return {
+        error: false,
+      };
+    }
+
+    return null;
+  }
+
   constructor(props) {
     super(props);
     this.state = {
       error: false,
-    };
-  }
-
-  static getDerivedStateFromProps() {
-    return {
-      error: false,
+      location: props.location,
     };
   }
 
