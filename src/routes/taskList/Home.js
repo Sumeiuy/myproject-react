e308ec456d2bab2f399bf19faa3a6f0526e96bcc
K0240008 <wangjunjun40829@billjc.com>
2018-09-03 17:12:14 +0800

    #FSP-7535#eslint错误修改

--- src/routes/taskList/Home.js
+++ src/routes/taskList/Home.js
@@ -2,7 +2,7 @@
  * @Author: sunweibin
  * @Date: 2018-04-13 11:57:34
  * @Last Modified by: WangJunJun
- * @Last Modified time: 2018-08-28 13:59:43
+ * @Last Modified time: 2018-09-03 17:11:30
  * @description 任务管理首页
  */
 
@@ -35,7 +35,6 @@ import {
   STATUS_MANAGER_VIEW,
   SYSTEMCODE,
   STATE_EXECUTE_CODE,
-  STATE_ALL_CODE,
   // 三个视图左侧任务列表的请求入参，在config里面配置，后续如果需要新增，或者删除某个param，
   // 请在config里面配置QUERY_PARAMS
   QUERY_PARAMS,
@@ -727,23 +726,6 @@ export default class PerformerView extends PureComponent {
       ...finalPostData,
       ...this.addSortParam(currentViewType, query),
     };
-    // 执行者视图中，状态默认选中‘执行中’, status传50
-    // url中status为‘all’时传空字符串或者不传，其余传对应的code码
-    if (this.isExecutorView(currentViewType)) {
-      if (status) {
-        finalPostData.status = status === STATE_ALL_CODE ? '' : status;
-      } else {
-        finalPostData.status = STATE_EXECUTE_CODE;
-      }
-    } else if (_.includes([INITIATOR, CONTROLLER], currentViewType)) {
-      // 创建者视图和管理者视图中，状态默认选中‘所有状态’， status传空字符串或者不传
-      // url中status为‘all’时传空字符串或者不传，其余传对应的code码
-      if (!status || status === STATE_ALL_CODE) {
-        finalPostData.status = '';
-      } else {
-        finalPostData.status = status;
-      }
-    }
     // 状态默认选中‘执行中’, status传50，其余传对应的code码
     finalPostData.status = status || STATE_EXECUTE_CODE;
     finalPostData = { ...finalPostData, missionViewType: currentViewType };
