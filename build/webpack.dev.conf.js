dca91114587a6d78b25846dfb6211b8166638244
K0050051 <k0050051@test.htsc.com.cn>
2018-09-12 15:19:47 +0800

    #FSP-11234#修正babel配置

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -104,7 +104,16 @@ module.exports = merge(baseWebpackConfig, {
       loaders: [{
         loader: 'babel-loader',
         options: {
-          presets: ["@babel/preset-react"],
+          presets: [
+            [
+              "@babel/preset-env", {
+                "targets": {
+                  "browsers": ["ie >= 10", "chrome >= 45"]
+                },
+                "modules": false
+              }],
+            "@babel/preset-react"
+          ],
           plugins: [
             [
               "import", {
@@ -112,9 +121,29 @@ module.exports = merge(baseWebpackConfig, {
                 "style": true
               }
             ],
+            ["@babel/plugin-transform-runtime", { "corejs": 2 }],
+            ["@babel/plugin-proposal-decorators", { "legacy": true }],
             ["@babel/plugin-proposal-class-properties", { "loose": true }],
-            ["@babel/plugin-proposal-object-rest-spread"],
-          ]
+            "@babel/plugin-proposal-object-rest-spread",
+            "@babel/plugin-syntax-dynamic-import",
+            "@babel/plugin-proposal-nullish-coalescing-operator",
+            "@babel/plugin-proposal-optional-chaining"
+          ],
+          env: {
+            "production": {},
+            "development": {
+              "plugins": [
+                [
+                  "dva-hmr", {
+                    "container": ".react-app",
+                    "quiet": false
+                  },
+                  "@babel/plugin-transform-react-jsx-source"
+                ]
+              ]
+            }
+          },
+          comments: true
         }
       }],
     }),
