8c229fa2db430e7f695db382e95ed5fd9a2d7c39
K0050051 <k0050051@test.htsc.com.cn>
2018-09-14 15:56:11 +0800

    #FSP-14623#新版首页图表tab组件实现

---
+++ src/components/newHome/ChartsTab.js
@@ -0,0 +1,245 @@
+/**
+ * @Author: zhufeiyang
+ * @Date: 2018-01-30 13:37:45
+ * @Last Modified by: zhufeiyang
+ * @Last Modified time: 2018-09-14 13:46:09
+ */
+
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import { Tabs } from 'antd';
+import { autobind } from 'core-decorators';
+import _ from 'lodash';
+import { optionsMap } from '../../config';
+import {
+  CUST_MANAGER,
+  ORG,
+  MAIN_MAGEGER_ID,
+} from '../../routes/customerPool/config';
+import { emp, time, permission } from '../../helper';
+import { transformDateTypeToDate } from '../customerPool/helper';
+import { PerformanceIndicators } from '../customerPool/home';
+import TabController from './TabController';
+import logable from '../../decorators/logable';
+
+const TabPane = Tabs.TabPane;
+
+export default class ChartsTab extends PureComponent {
+  static propTypes = {
+    location: PropTypes.object.isRequired,
+    custRange: PropTypes.array.isRequired,
+    cycle: PropTypes.array.isRequired,
+    managerIndicators: PropTypes.object.isRequired,
+    empInfo: PropTypes.object.isRequired,
+    performanceIndicators: PropTypes.object.isRequired,
+    custCount: PropTypes.oneOfType([
+      PropTypes.object,
+      PropTypes.array,
+    ]).isRequired,
+    getCustCount: PropTypes.func.isRequired,
+    getManagerIndicators: PropTypes.func.isRequired,
+    getPerformanceIndicators: PropTypes.func.isRequired,
+  }
+
+  static contextTypes = {
+    push: PropTypes.func.isRequired,
+    replace: PropTypes.func.isRequired,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: props.location,
+    };
+    // 初始化当前登录用户的orgId
+    this.loginOrgId = emp.getOrgId();
+    // HTSC 首页指标查询
+    this.hasIndexViewPermission = permission.hasIndexViewPermission();
+  }
+
+  componentDidMount() {
+    this.requstIndicator(this.props);
+  }
+
+  componentDidUpdate(prevProps) {
+  
+    const {
+      location: { query: nextQuery },
+    } = this.props;
+
+    const {
+      location: { query: prevQuery },
+    } = prevProps;
+
+    // 时间或者组织机构树变化
+    // 重新请求绩效指标数据和净创收数据
+    if (prevQuery.orgId !== nextQuery.orgId
+      || prevQuery.cycleSelect !== nextQuery.cycleSelect) {
+      // 请求指标数据
+      this.requstIndicator(this.props);
+    }
+  }
+
+  // 我的客户时custType=CUST_MANAGER，非我的客户时custType=ORG， custType用来传给后端
+  getCustType(orgId) {
+    let custType = CUST_MANAGER;
+    if (orgId) {
+      custType = orgId !== MAIN_MAGEGER_ID ? ORG : CUST_MANAGER;
+    } else if (this.hasIndexViewPermission) {
+      custType = ORG;
+    }
+    return custType;
+  }
+
+  // 周期类型
+  getDateType(cycleSelect) {
+    const { cycle } = this.props;
+    return cycleSelect || (!_.isEmpty(cycle) ? cycle[0].key : '');
+  }
+
+  getTimeSelectBeginAndEnd(props) {
+    const { cycle, location: { query: { cycleSelect } } } = props;
+    const { historyTime, customerPoolTimeSelect } = optionsMap;
+    const currentSelect = _.find(historyTime, (itemData) => {
+      const matchTime = _.find(
+        customerPoolTimeSelect,
+        item => item.key === (cycleSelect || (cycle[0] || {}).key),
+      ) || {};
+      return itemData.name === matchTime.name;
+    }) || {}; // 本月
+    const nowDuration = time.getDurationString(currentSelect.key);
+    const begin = nowDuration.begin;
+    const end = nowDuration.end;
+    return {
+      begin,
+      end,
+    };
+  }
+
+  requstIndicator(props) {
+    const {
+      cycle,
+      location: {
+        query: {
+          cycleSelect,
+          orgId,
+        },
+      },
+    } = props;
+
+    // 根据cycle获取对应的begin和end值
+    // T-1天的数据
+    const { begin, end } = this.getTimeSelectBeginAndEnd(props);
+    const custType = this.getCustType(orgId);
+    const tempObj = {
+      begin,
+      end,
+      custType,
+      cycleSelect: cycleSelect || (cycle[0] || {}).key,
+    };
+    if (orgId) {
+      tempObj.orgId = orgId !== MAIN_MAGEGER_ID ? orgId : '';
+    } else if (this.hasIndexViewPermission) {
+      tempObj.orgId = this.loginOrgId;
+    }
+    // 绩效指标
+    this.getIndicators(tempObj);
+  }
+
+  @autobind
+  getIndicators({ begin, end, orgId, cycleSelect, custType }) {
+    const {
+      getPerformanceIndicators,
+      getManagerIndicators,
+      getCustCount,
+      empInfo = {},
+    } = this.props;
+    const { tgQyFlag = false } = empInfo.empInfo || {};
+    const cycleDate = transformDateTypeToDate(cycleSelect);
+    const param = {
+      custType, // 客户范围类型
+      orgId, // 组织ID
+      dateType: this.getDateType(cycleSelect), // 周期类型
+      dateStart: cycleDate.cycleStartTime,
+      dateEnd: cycleDate.cycleEndTime,
+      empId: emp.getId(),
+    };
+    // 经营指标新增客户数指标
+    getCustCount({ ...param });
+    // 经营指标
+    getManagerIndicators({ ...param, end, begin });
+    // 查看投顾绩效开关:empinfo返回的权限指标字段（tgQyFlag：bool）
+    if (tgQyFlag) {
+      getPerformanceIndicators({ ...param, end, begin });
+    }
+  }
+
+  @autobind
+  @logable({ type: 'Click', payload: { name: '切换Tab：经营指标/投顾绩效' } })
+  handleTabClick() {} // 只是用来上报日志
+
+  renderTabController() {
+    const tabControllerProps = {
+      loginOrgId: this.loginOrgId,
+      custRange: this.props.custRange,
+      location: this.props.location,
+      replace: this.context.replace,
+      authority: this.hasIndexViewPermission,
+      cycle: this.props.cycle,
+    };
+    return (
+      <TabController {...tabControllerProps} />
+    );
+  }
+
+  render() {
+    const {
+      empInfo,
+      custCount,
+      managerIndicators,
+      performanceIndicators,
+      location,
+      cycle
+    } = this.props;
+
+    const { push } = this.context;
+    const { tgQyFlag } = (empInfo && empInfo.empInfo) || {};
+
+    return (
+      <Tabs
+        tabBarExtraContent={this.renderTabController()}
+        defaultActiveKey="manage"
+        onTabClick={this.handleTabClick}
+      >
+        <TabPane tab="经营指标" key="manage">
+          <PerformanceIndicators
+            empInfo={empInfo}
+            push={push}
+            custCount={custCount}
+            indicators={managerIndicators}
+            location={location}
+            cycle={cycle}
+            category={'manager'}
+            authority={this.hasIndexViewPermission}
+          />
+        </TabPane>
+        {
+          tgQyFlag ? (
+            <TabPane tab="投顾绩效" key="performance">
+              <PerformanceIndicators
+                empInfo={empInfo}
+                push={push}
+                indicators={performanceIndicators}
+                location={location}
+                cycle={cycle}
+                custCount={custCount}
+                category={'performance'}
+                authority={this.hasIndexViewPermission}
+              />
+            </TabPane>
+          ) : null
+        }
+      </Tabs>
+    );
+  }
+}
---
+++ src/components/newHome/TabController.js
@@ -0,0 +1,188 @@
+/**
+ * @Author: zhufeiyang
+ * @Date: 2018-01-30 13:37:45
+ * @Last Modified by: zhufeiyang
+ * @Last Modified time: 2018-09-14 13:46:09
+ */
+
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import _ from 'lodash';
+import { autobind } from 'core-decorators';
+import logable from '../../decorators/logable';
+import CustRange from '../customerPool/list/manageFilter/CustFilter';
+import { SingleFilter } from 'lego-react-filter/src';
+import { MAIN_MAGEGER_ID } from '../../routes/customerPool/config';
+import styles from './tabController.less';
+
+export default class TabController extends PureComponent {
+  static propTypes = {
+    loginOrgId: PropTypes.string.isRequired,
+    custRange: PropTypes.array.isRequired,
+    location: PropTypes.object.isRequired,
+    replace: PropTypes.func.isRequired,
+    authority: PropTypes.bool.isRequired,
+    cycle: PropTypes.array.isRequired,
+  }
+
+  custRange = this.getCustRange()
+
+  // 获取可以选择的营业部数据
+  getCustRange() {
+    const { custRange, loginOrgId, authority } = this.props;
+    const myCustomer = {
+      id: MAIN_MAGEGER_ID,
+      name: '我的客户',
+    };
+
+    // 如果有权限，可以选择当前登陆用户相应的营业部
+    if (authority) {
+      if (loginOrgId === (custRange[0] || {}).id) {
+        return [
+          ...custRange,
+          myCustomer,
+        ];
+      }
+      const currentOrgIdInfo = _.find(custRange, item => item.id === loginOrgId);
+      if (currentOrgIdInfo) {
+        return [
+          currentOrgIdInfo,
+          myCustomer,
+        ];
+      }
+
+      let currentDepartment;
+      _.forEach(custRange, item => {
+        if (item.children && !_.isEmpty(item.children)) {
+          const targetValue = _.find(item.children, object => object.id === loginOrgId);
+          if (targetValue) {
+            currentDepartment = targetValue;
+            return false;
+          }
+          return true;
+        }
+      });
+
+      if (currentDepartment) {
+        return [
+          currentDepartment,
+          myCustomer,
+        ];
+      }
+    }
+
+    // 没权限只能选择自己
+    return [
+      myCustomer,
+    ];
+  }
+
+  getCurrentOrgId() {
+    const {
+      loginOrgId,
+      location,
+      authority,
+    } = this.props;
+
+    const {
+      query: { orgId },
+    } = location;
+
+    // 如果从query上能取到，直接使用
+    if(orgId) {
+      return orgId;
+    }
+
+    // 有权限时使用当前登陆的营业部，作为默认值
+    if(authority) {
+      return loginOrgId;
+    }
+
+    // 无权限时，自己作为默认值
+    return MAIN_MAGEGER_ID;
+
+  }
+
+  // 获取当前的选择时间范围
+  getCurrentTime() {
+    const {
+      cycle,
+      location,
+    } = this.props;
+
+    const {
+      query: {
+      cycleSelect,
+      },
+    } = location;
+
+    return cycleSelect || (_.isArray(cycle) ? (cycle[0] || {}) : {}).key;
+  }
+
+  @autobind
+  @logable({
+    type: 'DropdownSelect',
+    payload: {
+      name: '机构树',
+      value: '$args[0].orgId',
+    },
+  })
+  handleCustRangeChange(value) {
+    const { replace, location: { pathname, query } } = this.props;
+    const { orgId } = value;
+    replace({
+      pathname,
+      query: {
+        ...query,
+        orgId,
+      },
+    });
+  }
+
+  @autobind
+  @logable({
+    type: 'DropdownSelect',
+    payload: {
+      name: '时间周期',
+      value: '$args[0].value',
+    },
+  })
+  handleTimeSelectChange(item) {
+    const { replace, location: { pathname, query } } = this.props;
+    const selectValue = item.value;
+    replace({
+      pathname,
+      query: {
+        ...query,
+        cycleSelect: selectValue,
+      },
+    });
+  }
+
+  render() {
+    const custRangeProps = {
+      defaultFirst: true,
+      custRange: this.custRange,
+      orgId: this.getCurrentOrgId(),
+      expandAll: true,
+      updateQueryState: this.handleCustRangeChange,
+      isHideFilterName: true,
+    };
+    const timeSelectProps = {
+      useCustomerFilter: true,
+      data: this.props.cycle,
+      value: this.getCurrentTime(),
+      onChange: this.handleTimeSelectChange,
+    };
+    return (
+      <div className={styles.tabControllerContainer}>
+        <div className={styles.custRangeContainer}>
+          <CustRange {...custRangeProps} />
+        </div>
+        <div className={styles.timeSelectContainer}>
+          <SingleFilter {...timeSelectProps} />
+        </div>
+      </div>
+    );
+  }
+}
