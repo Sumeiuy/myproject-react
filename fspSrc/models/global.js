dad863431f866c8a470cfdb07a92ef17e5900aca
K0050051 <k0050051@test.htsc.com.cn>
2018-09-25 20:01:55 +0800

    #FSP-14623#获取当前登录人的接口为第一优先级请求

--- fspSrc/models/global.js
+++ fspSrc/models/global.js
@@ -5,8 +5,6 @@
  * @author zhufeiyang
  */
 
-import { common as api } from '../../src/api';
-
 export default {
   namespace: 'global',
   state: {
@@ -14,33 +12,9 @@ export default {
     menus: {},
   },
   effects: {
-    // 获取用户有权限查看的菜单
-    * getMenus({ payload }, { call, put }) {
-      const response = yield call(api.getMenus, payload);
-      yield put({
-        type: 'getMenusSuccess',
-        payload: response,
-      });
-    },
   },
   reducers: {
-    // 根据用户权限可以查看的菜单
-    getMenusSuccess(state, action) {
-      const { payload: { resultData = {} } } = action;
-      const { primaryMenu = [], secondaryMenu = [] } = resultData;
-      return {
-        ...state,
-        menus: {
-          primaryMenu,
-          secondaryMenu,
-        },
-      };
-    },
   },
   subscriptions: {
-    setup({ dispatch }) {
-      // 初始化查询到用户信息后，立即查询用户的菜单权限
-      dispatch({ type: 'getMenus' });
-    },
   },
 };
