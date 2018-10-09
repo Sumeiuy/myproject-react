e55fa43eaa483c7ad103c90a8400712883a115e5
K0100008 <K0100008@test.htsc.com.cn>
2018-10-09 16:20:20 +0800

    #FSP-15554#新增客户360详情的api和model

--- src/routes/customerDetail/Home.js
+++ src/routes/customerDetail/Home.js
@@ -2,28 +2,54 @@
  * @Author: zhufeiyang
  * @Date: 2018-01-30 13:37:45
  * @Last Modified by: sunweibin
- * @Last Modified time: 2018-10-09 16:12:29
+ * @Last Modified time: 2018-10-09 16:14:37
  */
 
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+import { autobind } from 'core-decorators';
 import { Tabs } from 'antd';
+
 import withRouter from '../../decorators/withRouter';
+
 import styles from './home.less';
 
 const TabPane = Tabs.TabPane;
+
 @withRouter
 export default class Home extends PureComponent {
-
   static propTypes = {
+    location: PropTypes.object.isRequired,
+    // 新版客户360详情中的概要信息
+    summaryInfo: PropTypes.object.isRequired,
+    // 清除Redux中的数据
+    clearReduxData: PropTypes.func.isRequired,
   }
 
   static defaultProps = {
   }
 
-  componentDidMount() {}
+  constructor(props) {
+    super(props);
+    this.state = {
+      // 当前的tab页面, 默认展示 账户信息 Tab页
+      activeTabKey: 'accountInfo',
+    };
+  }
+
+  componentDidMount() {
+
+  }
+
+  // 切换客户360详情页的Tab
+  @autobind
+  handleTabChange(activeTabKey) {
+    this.setState({ activeTabKey });
+  }
 
   render() {
+    const { activeTabKey } = this.state;
+
     return (
       <div className={styles.container}>
         <div className={styles.breadCrumb}>面包屑</div>
@@ -33,9 +59,10 @@ export default class Home extends PureComponent {
         </div>
         <div className={styles.tabContainer}>
           <Tabs
+            activeKey={activeTabKey}
             className={styles.tab}
             defaultActiveKey={'accountInfo'}
-            onTabClick={this.handleTabClick}
+            onChange={this.handleTabChange}
             animated={false}
             tabBarGutter={40}
           >
