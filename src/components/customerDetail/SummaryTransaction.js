80fa237e75ca46906a9391220278c4933d2956e1
K0100008 <K0100008@test.htsc.com.cn>
2018-10-18 14:52:27 +0800

    #FSP-15756#优化雷达图数据详情表格

--- src/components/customerDetail/SummaryTransaction.js
+++ src/components/customerDetail/SummaryTransaction.js
@@ -2,7 +2,7 @@
  * @Author: sunweibin
  * @Date: 2018-10-15 22:30:04
  * @Last Modified by: sunweibin
- * @Last Modified time: 2018-10-18 11:25:10
+ * @Last Modified time: 2018-10-18 13:56:40
  * @description 客户360详情交易数据展示模块
  */
 import React from 'react';
@@ -11,8 +11,8 @@ import _ from 'lodash';
 
 import MoneyCell from './MoneyCell';
 import RateCell from './RateCell';
-import { displayTransMoney } from '../customerDetailAccountInfo/utils';
-import { calcSameTimeRate, convertRate } from './utils';
+import { displayMoney } from '../customerDetailAccountInfo/utils';
+import { calcSameTimeRate } from './utils';
 import { number } from '../../helper';
 
 import styles from './summaryTransaction.less';
@@ -23,9 +23,9 @@ export default function SummaryTransaction(props) {
     return null;
   }
   // 总资产
-  const assetText = displayTransMoney(data.asset);
+  const assetText = displayMoney(data.asset);
   // 年日均资产
-  const yearAvgAssets = displayTransMoney(data.yearAvgAssets);
+  const yearAvgAssets = displayMoney(data.yearAvgAssets);
   // 年日均资产增长率
   const yearAvgAssetsRate = calcSameTimeRate(data.yearAvgAssets, data.lastYearAvgAssets);
   // 年收益率
@@ -33,21 +33,21 @@ export default function SummaryTransaction(props) {
   // 年收益率增长率
   const yearMaxCostRate = calcSameTimeRate(data.yearMaxCostRate, data.lastYearMaxCostRate);
   // 年股基交易量
-  const yearGjAmt = displayTransMoney(data.yearGjAmt);
+  const yearGjAmt = displayMoney(data.yearGjAmt);
    // 年股基交易量增长率
   const yearGjAmtRate = calcSameTimeRate(data.yearGjAmt, data.lastYearGjAmt);
   // 净佣金
-  const purRake = displayTransMoney(data.purRake);
+  const purRake = displayMoney(data.purRake);
   // 利息收入
-  const netIncome = displayTransMoney(data.netIncome);
+  const netIncome = displayMoney(data.netIncome);
   // 天天发
-  const ttfMtkVal = displayTransMoney(data.ttfMtkVal);
+  const ttfMtkVal = displayMoney(data.ttfMtkVal);
   // 资金余额
-  const cashAmt = displayTransMoney(data.cashAmt);
+  const cashAmt = displayMoney(data.cashAmt);
   // 年产品销量
-  const yearProdAmt = displayTransMoney(data.yearProdAmt);
+  const yearProdAmt = displayMoney(data.yearProdAmt);
   // 产品日均保有
-  const yearProdHold = displayTransMoney(data.yearProdHold);
+  const yearProdHold = displayMoney(data.yearProdHold);
   // 归集率
   const gjlRate = number.convertRate(data.gjlRate);
   // 股基佣金率
--- src/components/customerDetailAccountInfo/assetRadarHelper.js
+++ src/components/customerDetailAccountInfo/assetRadarHelper.js
@@ -2,28 +2,25 @@
  * @Author: sunweibin
  * @Date: 2018-10-12 15:08:16
  * @Last Modified by: sunweibin
- * @Last Modified time: 2018-10-17 14:58:05
+ * @Last Modified time: 2018-10-18 14:12:32
  * @description 资产分布的雷达图数据处理相关的辅助函数
  */
 import _ from 'lodash';
 
 // 将指标名称,指标索引和指标值合并起来，用于在渲染指标名称的时候使用echart的formatter属性
 export function composeIndicatorAndData(indicators, data) {
-  // 用于渲染雷达图的值
-  const renderValue = _.map(data, item => item.value);
   // 找出最大和最小值，并且分别设置雷达图上的最大和最小值区间范围，以便图表展示合理
-  let max = Math.max(...renderValue);
-  let min = Math.min(...renderValue);
+  let max = Math.max(...data);
+  let min = Math.min(...data);
   if (max === min) {
     // 如果最大值和最小值相同情况，则需要对页面数据展示做下优化处理
     max += 10;
   }
   return _.map(indicators, (indicator, index) => {
     const { name } = indicator;
-    const value = data[index].formatedValue || 0;
-    const unit = data[index].unit;
+    const value = data[index];
     return {
-      name: `${name}|${index}|${value}${unit}`,
+      name: `${name}|${index}|${value}`,
       max: Math.ceil(max * 1.1),
       min: Math.floor(min * 0.9),
     };
