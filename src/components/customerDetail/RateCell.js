6739f403a3642989893a3d015e6c25c0dd7088e7
K0100008 <K0100008@test.htsc.com.cn>
2018-10-19 10:26:32 +0800

    #FSP-15768#修改代码规范，把通用的部分迁移至单独的文件中

--- src/components/customerDetail/RateCell.js
+++ src/components/customerDetail/RateCell.js
@@ -2,7 +2,7 @@
  * @Author: sunweibin
  * @Date: 2018-10-16 08:50:17
  * @Last Modified by: sunweibin
- * @Last Modified time: 2018-10-18 17:52:12
+ * @Last Modified time: 2018-10-19 10:24:55
  * @description 新版客户360详情交易数据中比例展示Cell
  */
 import React from 'react';
@@ -12,7 +12,7 @@ import { Popover } from 'antd';
 
 import Icon from '../common/Icon';
 import { displayMoney } from '../customerDetailAccountInfo/utils';
-import { calcSameTimeRate } from './utils';
+import { calcSameTimeRate, getRateTipWhenLastEqual0, getRateTip } from './utils';
 import { number } from '../../helper';
 
 import styles from './cell.less';
@@ -20,57 +20,15 @@ import styles from './cell.less';
 export default function RateCell(props) {
   const { title, current, last, isMoney } = props;
   // 年收益率显示的百分比，所以在数据处理上需要做区分
-  let newCurrentText = current;
-  if (isMoney) {
-    newCurrentText = displayMoney(current);
-  } else {
-    newCurrentText = number.convertRate(current);
-  }
+  const newCurrentText = isMoney ? displayMoney(current) : number.convertRate(current);
   // 1.首先判断什么时候显示 Tip
-  let disPlayTip = true;
-  if (last !== 0 && calcSameTimeRate(current, last) === 0) {
-    // 当前去年同期值不等于 0 ，以及计算出来的增长率为0的时候，不显示提示
-    disPlayTip = false;
-  }
+  // 当前去年同期值不等于 0 ，以及计算出来的增长率为0的时候，不显示提示
+  let notDisPlayTip = last !== 0 && calcSameTimeRate(current, last) === 0;
   // 2. 展示的比率提示信息
-  let rateText = '';
-  let isAsc = true;
-  let tip = '';
-  if (last === 0 && current >= 0) {
-    rateText = '>999%';
-    isAsc = true;
-    tip = `去年同期0元，同比${rateText}`;
-  } else if (last ===0 && current < 0) {
-    rateText = '<-999%';
-    isAsc = false;
-    tip = `去年同期0元，同比${rateText}`;
-  } else if (last ===0 && current === 0) {
-    rateText = '0%';
-    isAsc = false;
-    tip = `去年同期0元，同比${rateText}`;
-  } else {
-    // 针对超大数据进行特殊处理
-    const rate = calcSameTimeRate(current, last);
-    const lastText = displayMoney(last);
-    let actionText = '';
-    if (rate > 10) {
-      rateText = '>999%';
-      isAsc = true;
-      actionText = '增长';
-    } else if (rate < -10) {
-      rateText = '<-999%';
-      isAsc = false;
-      actionText = '下跌';
-    } else {
-      rateText = number.convertRate(rate);
-      isAsc = rate >= 0;
-      actionText = isAsc ? '增长' : '下跌';
-    }
-    tip = `去年同期${lastText}元，同比${actionText}${rateText}`;
-  }
+  const tips = last === 0 ? getRateTipWhenLastEqual0(current) : getRateTip(last, current);
   const ascCls = cx({
-    [styles.asc]: isAsc,
-    [styles.desc]: !isAsc,
+    [styles.asc]: tips.isAsc,
+    [styles.desc]: !tips.isAsc,
   });
 
   return (
@@ -79,14 +37,14 @@ export default function RateCell(props) {
       <div className={styles.content}>
         <span>{newCurrentText}</span>
         {
-          !disPlayTip
+          notDisPlayTip
             ? null
             : (
               <span>
-                <span>{ isAsc ? (<Icon type="zhang" className={ascCls} />) : (<Icon type="die" className={ascCls} />) }</span>
-                <span className={ascCls}>{rateText}</span>
+                <span>{ tips.isAsc ? (<Icon type="zhang" className={ascCls} />) : (<Icon type="die" className={ascCls} />) }</span>
+                <span className={ascCls}>{tips.rateText}</span>
                 <span>
-                  <Popover overlayClassName={styles.labelPopover} content={tip} trigger="click">
+                  <Popover overlayClassName={styles.labelPopover} content={tips.tip} trigger="click">
                     <Icon className={styles.tishi} type="tishi"/>
                   </Popover>
                 </span>
