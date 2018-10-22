42824653d2df85cd8db18a34e8942b2304111961
K0100008 <K0100008@test.htsc.com.cn>
2018-10-22 17:01:35 +0800

    #FSP-15837#优化资产分布雷达图的数据展示

--- src/components/customerDetailAccountInfo/assetRadarHelper.js
+++ src/components/customerDetailAccountInfo/assetRadarHelper.js
@@ -2,10 +2,12 @@
  * @Author: sunweibin
  * @Date: 2018-10-12 15:08:16
  * @Last Modified by: sunweibin
- * @Last Modified time: 2018-10-22 14:32:07
+ * @Last Modified time: 2018-10-22 16:39:54
  * @description 资产分布的雷达图数据处理相关的辅助函数
  */
 import _ from 'lodash';
+
+import { RADAR_MUST_DISPLAY_INDICATORS } from './config';
 
 // 将指标名称,指标索引和指标值合并起来，用于在渲染指标名称的时候使用echart的formatter属性
 export function composeIndicatorAndData(indicators, data) {
@@ -21,3 +23,12 @@ export function composeIndicatorAndData(indicators, data) {
     };
   });
 };
+
+// 根据需求当除了现金、股票、债券、理财、基金五项指标之外的指标，
+// 如果其指标值为 0 的话，则不展示该指标值
+export function pickRadarDisplayData(data) {
+  return _.filter(data, item => {
+    const { key, value } = item;
+    return value !== 0 || _.includes(RADAR_MUST_DISPLAY_INDICATORS, key);
+  });
+}
