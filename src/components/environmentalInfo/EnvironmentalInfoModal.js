a3bf37b51d21e3403e876e2ba6883620422c8024
K0380011 <529907451@qq.com>
2018-10-30 10:57:01 +0800

    #FSP-16019#客户端信息收集和展示

--- src/components/environmentalInfo/EnvironmentalInfoModal.js
+++ src/components/environmentalInfo/EnvironmentalInfoModal.js
@@ -2,7 +2,7 @@
  * @Author: zuoguangzu
  * @Date: 2018-10-29 13:17:52
  * @Last Modified by: zuoguangzu
- * @Last Modified time: 2018-10-30 10:19:48
+ * @Last Modified time: 2018-10-30 10:56:25
  */
 
 import React from 'react';
@@ -67,55 +67,6 @@ export default function EnvironmentalInfo(props) {
         </div>
       </div>
     </CommonModal>
-
-    // <Modal
-    //   title="环境信息"
-    //   width={650}
-    //   height={300}
-    //   className={styles.environmentalInfo}
-    //   destroyOnClose
-    //   visible={environmentalInfoVisible}
-    //   footer={null}
-    //   onCancel={handleEnvironmentalInfoHide}
-    // >
-    //   <div className={styles.environmentalInfoContent}>
-    //     <div className={styles.environmentalInfoImg}>
-    //       <img src={Icon}/>
-    //     </div>
-    //     <div className={styles.environmentalInfoText}>
-    //       <ul>
-    //         <li>
-    //           <span>操作系统：</span>
-    //           <span>操作系统</span>
-    //         </li>
-    //         <li>
-    //           <span>屏幕高度：</span>
-    //           <span>屏幕高度</span>
-    //         </li>
-    //         <li>
-    //           <span>浏览器名：</span>
-    //           <span>浏览器名</span>
-    //         </li>
-    //       </ul>
-    //     </div>
-    //     <div className={styles.environmentalInfoText}>
-    //       <ul>
-    //         <li>
-    //           <span>操作系统版本：</span>
-    //           <span>操作系统版本</span>
-    //         </li>
-    //         <li>
-    //           <span>屏幕宽度：</span>
-    //           <span>屏幕宽度</span>
-    //         </li>
-    //         <li>
-    //           <span>浏览器版本：</span>
-    //           <span>浏览器版本</span>
-    //         </li>
-    //       </ul>
-    //     </div>
-    //   </div>
-    // </Modal>
   );
 }
 
