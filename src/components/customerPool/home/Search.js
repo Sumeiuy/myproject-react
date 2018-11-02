4a759e94611c6e55c192aa91a8fea684abeb4e3d
k0050045 <xuwenkang@chinasoftinc.com>
2018-11-02 15:53:11 +0800

    #FSP-16003# 前端淘客页面增加客户搜索功能 头部搜索框增加每次搜索后清空value功能

--- src/components/customerPool/home/Search.js
+++ src/components/customerPool/home/Search.js
@@ -90,6 +90,10 @@ export default class Search extends PureComponent {
     this.props.saveSearchVal({
       searchVal: this.state.value,
     });
+    // 搜索后跳转到客户列表页面时清空搜索框的值，统一交由客户列表页面的搜索框来处理
+    this.setState({
+      value: '',
+    });
     // 有标签描述需要将描述存到storage
     if (labelDesc) {
       store.set(`${options.labelMapping}-labelDesc`, {
@@ -210,6 +214,14 @@ export default class Search extends PureComponent {
       },
     });
     this.handleOpenTab(query);
+    // 由于AutoComplete组件onSelect触发之后，会在onSelect里动作全部执行完之后自动触发onChange -> setState, 将选中的option的value值置为当前组件的value
+    // 加定时器是利用事件循环将此处的setState执行顺序放到onSelect自动触发的setState之后,
+    // 以达到选择下拉框之后清空搜索框value的效果。
+    setTimeout(() => {
+      this.setState({
+        value: '',
+      });
+    }, 0);
   }
 
   @autobind
@@ -361,6 +373,7 @@ export default class Search extends PureComponent {
       showMoreLabelModal,
       isOnlySearchable,
     } = this.props;
+    const { value } = this.state;
     const autoCompleteOption = isPreview ? {} :
     {
       dataSource: this.renderDatasource(),
@@ -368,6 +381,7 @@ export default class Search extends PureComponent {
       onSearch: this.handleSearch,
       onChange: this.handleChange,
       defaultValue: searchHistoryVal,
+      value,
     };
 
     const trueStyles = isOnlySearchable ? classes : styles;
