b32867e1bd20bcca0874550510a9d6c19e19f797
K0050051 <k0050051@test.htsc.com.cn>
2018-11-08 16:45:29 +0800

    #FSP-15010#修正jsp页面在新系统下的关闭问题

--- src/config/index.js
+++ src/config/index.js
@@ -27,6 +27,7 @@ import padSightLabelDesc from './constructSightLabelDesc';
 import * as createTaskEntry from './createTaskEntry';
 import seperator from './filterSeperator';
 import retTabParam from './retTabParam';
+import tabMenuConfig from './tabMenu';
 
 const sessionStore = engine.createStore([sessionStorage, cookieStorage]);
 
@@ -58,4 +59,5 @@ export {
   createTaskEntry,
   seperator,
   retTabParam,
+  tabMenuConfig,
 };
