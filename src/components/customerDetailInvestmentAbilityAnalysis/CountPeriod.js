72a26d206b50a3f63798964b0f532968efd383d3
K0170222 <zhangjun@git.com.cn>
2018-11-22 09:18:35 +0800

    #FSP-16531#统计期间修改成字符串模板格式

--- src/components/customerDetailInvestmentAbilityAnalysis/CountPeriod.js
+++ src/components/customerDetailInvestmentAbilityAnalysis/CountPeriod.js
@@ -2,7 +2,7 @@
  * @Author: zhangjun
  * @Date: 2018-11-20 14:32:56
  * @Last Modified by: zhangjun
- * @Last Modified time: 2018-11-20 15:07:28
+ * @Last Modified time: 2018-11-22 09:16:24
  * @description 统计期间
  */
 
@@ -11,14 +11,17 @@ import { endDateOfLastMonth, lastYearDataOfLastMonth } from './config';
 
 import styles from './countPeriod.less';
 
+const COUNT_PERIOD = `${lastYearDataOfLastMonth}-${endDateOfLastMonth}(一年期)`;
+
 export default function CountPeriod(props) {
+
   return (
     <div className={styles.countPeriod}>
       <span className={styles.label}>
         统计期间：
       </span>
       <span className={styles.value}>
-      {lastYearDataOfLastMonth}-{endDateOfLastMonth}(一年期)
+        {COUNT_PERIOD}
       </span>
     </div>
   );
