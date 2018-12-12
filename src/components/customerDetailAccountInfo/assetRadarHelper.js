5edac92c85bebb51d1818335a889632750c68977
K0100008 <K0100008@test.htsc.com.cn>
2018-12-12 13:12:10 +0800

    #FSP-17203#修改资产分布详情数据展示，雷达图顺时针排列指标

--- src/components/customerDetailAccountInfo/assetRadarHelper.js
+++ src/components/customerDetailAccountInfo/assetRadarHelper.js
@@ -2,12 +2,10 @@
  * @Author: sunweibin
  * @Date: 2018-10-12 15:08:16
  * @Last Modified by: sunweibin
- * @Last Modified time: 2018-11-23 11:50:42
+ * @Last Modified time: 2018-12-12 13:08:26
  * @description 资产分布的雷达图数据处理相关的辅助函数
  */
 import _ from 'lodash';
-
-import { RADAR_MUST_DISPLAY_INDICATORS } from './config';
 
 // 将指标名称,指标索引和指标值合并起来，用于在渲染指标名称的时候使用echart的formatter属性
 export function composeIndicatorAndData(indicators, data) {
@@ -30,11 +28,37 @@ export function composeIndicatorAndData(indicators, data) {
   });
 }
 
+// 因为需求要求资产分布的雷达图按照顺时针展示
+// 但是实际上echarts的雷达图均是逆时针方向填写各个指标
+// 因此需要配置指标的展示顺序
+const indicatorsOrder = [
+  { name: '开基', key: 'PA050000', isMust: true },
+  { name: '权证', key: 'PA020000' },
+  { name: '回购', key: 'PA080000' },
+  { name: '债券', key: 'PA030000', isMust: true },
+  { name: 'OTC', key: 'PA090000' },
+  { name: '现金', key: '99', isMust: true },
+  { name: '私募', key: 'PA100000' },
+  { name: '理财', key: 'PA070000', isMust: true },
+  { name: '股票', key: 'PA040000', isMust: true },
+];
+
 // 根据需求当除了现金、股票、债券、理财、基金五项指标之外的指标，
 // 如果其指标值为 0 的话，则不展示该指标值
 export function pickRadarDisplayData(data) {
-  return _.filter(data, (item) => {
-    const { key, value } = item;
-    return value !== 0 || _.includes(RADAR_MUST_DISPLAY_INDICATORS, key);
+  if (_.isEmpty(data)) {
+    return [];
+  }
+  const orderedData = _.map(indicatorsOrder, (indicator) => {
+    const { key } = indicator;
+    const dataItem = _.find(data, item => item.key === key) || {};
+    return {
+      ...indicator,
+      value: dataItem.value || 0,
+    };
+  });
+  return _.filter(orderedData, (item) => {
+    const { value, isMust = false } = item;
+    return value !== 0 || isMust;
   });
 }
