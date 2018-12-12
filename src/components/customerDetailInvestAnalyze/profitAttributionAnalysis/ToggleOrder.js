027da802b1c8974cdbb769ae6701d38dc854ae8a
K0380011 <529907451@qq.com>
2018-12-12 14:25:32 +0800

    #FSP-111#个股收益明细

--- src/components/customerDetailInvestAnalyze/profitAttributionAnalysis/ToggleOrder.js
+++ src/components/customerDetailInvestAnalyze/profitAttributionAnalysis/ToggleOrder.js
@@ -2,7 +2,7 @@
  * @Author: zuoguangzu
  * @Date: 2018-12-07 17:14:55
  * @Last Modified by: zuoguangzu
- * @Last Modified time: 2018-12-11 18:51:18
+ * @Last Modified time: 2018-12-12 10:27:18
  * @description 切换排序方式
  */
 
@@ -38,13 +38,14 @@ export default class ToggleOrder extends PureComponent {
     },
   })
   handleToggleOrder() {
-    const { isUp } = this.state;
     this.setState(prevState => ({
       isUp: !prevState.isUp,
-    }));
-    // 排序（01：升序；02：降序）
-    const orderType = isUp ? '01' : '02';
-    this.props.onOrderChange(orderType);
+    }), () => {
+      const { isUp } = this.state;
+      // 排序（01：升序；02：降序）
+      const orderType = isUp ? '01' : '02';
+      this.props.onOrderChange(orderType);
+    });
   }
 
   render() {
